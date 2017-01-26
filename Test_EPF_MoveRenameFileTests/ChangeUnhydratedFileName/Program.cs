e7547163490bc11f3b79c7139b12da06a7091d59
William Baker <wilbaker@microsoft.com>
2017-01-25 17:58:21 -0800

    Add more test files

---
+++ Test_EPF_MoveRenameFileTests/ChangeUnhydratedFileName/Program.cs
@@ -0,0 +1,28 @@
+﻿using NUnitLite;
+using System;
+using System.Threading;
+
+namespace GVFS.StressTests
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] test_args = args;
+
+            for (int i = 0; i < Properties.Settings.Default.TestRepeatCount; i++)
+            {
+                Console.WriteLine("Starting pass {0}", i + 1);
+                DateTime now = DateTime.Now;
+                new AutoRun().Execute(test_args);
+                Console.WriteLine("Completed pass {0} in {1}", i + 1, DateTime.Now - now);
+                Console.WriteLine();
+
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+            }
+
+            Console.WriteLine("All tests completed.  Press Enter to exit.");
+            Console.ReadLine();
+        }
+    }
+}
