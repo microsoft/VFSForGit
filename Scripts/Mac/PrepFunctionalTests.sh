8505a6a77885c56ffa076003905e4aad96a670f8
Kevin Willford <kewillf@microsoft.com>
2018-08-13 12:18:21 -0600

    Use the test repo PAT set by the build

--- Scripts/Mac/PrepFunctionalTests.sh
+++ Scripts/Mac/PrepFunctionalTests.sh
@@ -43,8 +43,8 @@ fi
 git config --global credential.helper "!/Library/Java/JavaVirtualMachines/jdk-$JAVAVERSION.jdk/Contents/Home/bin/java -Ddebug=false --add-modules java.xml.bind -Djava.net.useSystemProxies=true -jar /usr/local/Cellar/git-credential-manager/2.0.3/libexec/git-credential-manager-2.0.3.jar" || exit 1
 
 # If we're running on an agent where the PAT environment variable is set and a URL is passed into the script, add it to the keychain.
-PAT=$SYSTEM_ACCESSTOKEN
 PATURL=$1
+PAT=$2
 if [[ ! -z $PAT && ! -z $PATURL ]] ; then
     security add-generic-password -a "Personal Access Token" -s "gcm4ml:git:$PATURL" -D Credential -w $PAT || exit 1
 fi
