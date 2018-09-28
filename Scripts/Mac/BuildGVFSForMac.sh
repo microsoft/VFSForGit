b3400aef8a068cd7b5c726052a4d12ea1e30c857
Saeed Noursalehi <sanoursa@microsoft.com>
2018-09-28 15:38:10 -0700

    Fix build after Xcode upgrade

--- Scripts/Mac/BuildGVFSForMac.sh
+++ Scripts/Mac/BuildGVFSForMac.sh
@@ -44,7 +44,7 @@ dotnet build $SRCDIR/GVFS.sln --runtime osx-x64 --framework netcoreapp2.1 --conf
 dotnet publish $SRCDIR/GVFS.sln /p:Configuration=$DOTNETCONFIGURATION /p:Platform=x64 --runtime osx-x64 --framework netcoreapp2.1 --self-contained --output $PUBLISHDIR /maxcpucount:1 || exit 1
 
 NATIVEDIR=$SRCDIR/GVFS/GVFS.Native.Mac
-xcodebuild -sdk macosx10.13 -configuration $CONFIGURATION -workspace $NATIVEDIR/GVFS.Native.Mac.xcworkspace build -scheme GVFS.Native.Mac -derivedDataPath $ROOTDIR/BuildOutput/GVFS.Native.Mac || exit 1
+xcodebuild -configuration $CONFIGURATION -workspace $NATIVEDIR/GVFS.Native.Mac.xcworkspace build -scheme GVFS.Native.Mac -derivedDataPath $ROOTDIR/BuildOutput/GVFS.Native.Mac || exit 1
 
 echo 'Copying native binaries to Publish directory'
 cp $BUILDOUTPUT/GVFS.Native.Mac/Build/Products/$CONFIGURATION/GVFS.ReadObjectHook $PUBLISHDIR || exit 1
