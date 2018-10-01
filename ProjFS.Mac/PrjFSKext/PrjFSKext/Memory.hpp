e50e488ede2723162593b9a316a83915d29b1dae
Phil Dennis-Jordan <phil@philjordan.eu>
2018-10-01 13:52:41 +0200

    Mac kext: Adds typed array memory allocation function.

--- ProjFS.Mac/PrjFSKext/PrjFSKext/Memory.hpp
+++ ProjFS.Mac/PrjFSKext/PrjFSKext/Memory.hpp
@@ -7,4 +7,17 @@ kern_return_t Memory_Cleanup();
 void* Memory_Alloc(uint32_t size);
 void Memory_Free(void* buffer, uint32_t size);
 
+template <typename T>
+T* Memory_AllocArray(uint32_t arrayLength)
+{
+    size_t allocBytes = arrayLength * sizeof(T);
+    if (allocBytes > UINT32_MAX)
+    {
+        return nullptr;
+    }
+    
+    return static_cast<T*>(Memory_Alloc(static_cast<uint32_t>(allocBytes)));
+}
+
+
 #endif /* Memory_h */
