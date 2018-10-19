5ed76cbe3b4d6116855ce827aeae439310a246db
Chris Darroch <chrisd@apache.org>
2018-10-19 15:41:26 -0700

    Managed interface for Linux projected filesystem
    Add an initial Linux implementation of a VirtualizationInstance
    with the same types, callbacks, and functions as the Mac interface.
    
    The underlying dynamic library used by the interoperability layer
    is not included here due to anticipated licensing requirements,
    but would be provided separately.
    
    Note that it may be valuable to consider a future refactoring of
    identical classes between the Linux and Mac implementations (such
    as PrjFSLib.Linux.Result and PrjFSLib.Mac.Result).

---
+++ ProjFS.Linux/PrjFSLib.Linux.Managed/Interop/PrjFSLib.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace PrjFSLib.Linux.Interop
+{
+    internal static class PrjFSLib
+    {
+        // TODO(Linux): set value from that defined in Linux library header
+        public const int PlaceholderIdLength = 128;
+        private const string PrjFSLibPath = "libprojfs.so";
+
+        // TODO(Linux): revise library functions for Linux
+        [DllImport(PrjFSLibPath, EntryPoint = "PrjFS_StartVirtualizationInstance")]
+        public static extern Result StartVirtualizationInstance(
+            string virtualizationRootFullPath,
+            Callbacks callbacks,
+            uint poolThreadCount);
+
+        [DllImport(PrjFSLibPath, EntryPoint = "PrjFS_ConvertDirectoryToVirtualizationRoot")]
+        public static extern Result ConvertDirectoryToVirtualizationRoot(
+            string virtualizationRootFullPath);
+
+        [DllImport(PrjFSLibPath, EntryPoint = "PrjFS_WritePlaceholderDirectory")]
+        public static extern Result WritePlaceholderDirectory(
+            string relativePath);
+
+        [DllImport(PrjFSLibPath, EntryPoint = "PrjFS_WritePlaceholderFile")]
+        public static extern Result WritePlaceholderFile(
+            string relativePath,
+            [MarshalAs(UnmanagedType.LPArray, SizeConst = PlaceholderIdLength)]
+            byte[] providerId,
+            [MarshalAs(UnmanagedType.LPArray, SizeConst = PlaceholderIdLength)]
+            byte[] contentId,
+            ulong fileSize,
+            ushort fileMode);
+
+        [DllImport(PrjFSLibPath, EntryPoint = "PrjFS_WriteSymLink")]
+        public static extern Result WriteSymLink(
+            string relativePath,
+            string symLinkTarget);
+
+        [DllImport(PrjFSLibPath, EntryPoint = "PrjFS_UpdatePlaceholderFileIfNeeded")]
+        public static extern Result UpdatePlaceholderFileIfNeeded(
+            string relativePath,
+            [MarshalAs(UnmanagedType.LPArray, SizeConst = PlaceholderIdLength)]
+            byte[] providerId,
+            [MarshalAs(UnmanagedType.LPArray, SizeConst = PlaceholderIdLength)]
+            byte[] contentId,
+            ulong fileSize,
+            ushort fileMode,
+            UpdateType updateType,
+            ref UpdateFailureCause failureCause);
+
+        [DllImport(PrjFSLibPath, EntryPoint = "PrjFS_ReplacePlaceholderFileWithSymLink")]
+        public static extern Result ReplacePlaceholderFileWithSymLink(
+            string relativePath,
+            string symLinkTarget,
+            UpdateType updateType,
+            ref UpdateFailureCause failureCause);
+
+        [DllImport(PrjFSLibPath, EntryPoint = "PrjFS_DeleteFile")]
+        public static extern Result DeleteFile(
+            string relativePath,
+            UpdateType updateType,
+            ref UpdateFailureCause failureCause);
+
+        [DllImport(PrjFSLibPath, EntryPoint = "PrjFS_WriteFileContents")]
+        public static extern Result WriteFileContents(
+            IntPtr fileHandle,
+            IntPtr bytes,
+            uint byteCount);
+    }
+}
---
+++ ProjFS.Linux/PrjFSLib.Linux.Managed/NotificationType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PrjFSLib.Linux
+{
+    [Flags]
+    public enum NotificationType
+    {
+        Invalid             = 0x00000000,
+
+        None                = 0x00000001,
+        NewFileCreated      = 0x00000004,
+        PreDelete           = 0x00000010,
+        FileRenamed         = 0x00000080,
+        HardLinkCreated     = 0x00000100,
+        PreConvertToFull    = 0x00001000,
+
+        PreModify           = 0x10000001,
+        FileModified        = 0x10000002,
+        FileDeleted         = 0x10000004,
+    }
+}
