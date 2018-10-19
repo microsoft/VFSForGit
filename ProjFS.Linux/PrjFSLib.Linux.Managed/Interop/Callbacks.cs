7191639f3ec413d77374ef97c33d4e69c30301bb
Chris Darroch <chrisd@apache.org>
2018-10-19 15:41:26 -0700

    Managed interface for Linux projected filesystem
    Add an initial Linux implementation of a VirtualizationInstance
    with the same types, callbacks, and functions as the Mac interface.
    
    The underlying dynamic library used by the interoperability layer
    is not included here due to anticipated licensing requirements,
    but would be provided separately.
    
    Note that it may be valuable to consider a future refactoring of
    identical classes between the Linux and Mac implementations (such
    as PrjFSLib.Linux.Result and PrjFSLib.Mac.Result).

---
+++ ProjFS.Linux/PrjFSLib.Linux.Managed/Interop/Callbacks.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.InteropServices;
+
+namespace PrjFSLib.Linux.Interop
+{
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct Callbacks
+    {
+        public EnumerateDirectoryCallback OnEnumerateDirectory;
+        public GetFileStreamCallback OnGetFileStream;
+        public NotifyOperationCallback OnNotifyOperation;
+    }
+}
