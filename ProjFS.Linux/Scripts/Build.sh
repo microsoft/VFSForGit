7e40188ee27ed6f54aad25d96de7d2f9dcaa61b0
Chris Darroch <chrisd@apache.org>
2018-10-19 15:41:26 -0700

    Managed interface for Linux projected filesystem
    Add an initial Linux implementation of a VirtualizationInstance
    with the same types, callbacks, and functions as the Mac interface.
    
    The underlying dynamic library used by the interoperability layer
    is not included here due to anticipated licensing requirements,
    but would be provided separately.
    
    Note that it may be valuable to consider a future refactoring of
    identical classes between the Linux and Mac implementations (such
    as PrjFSLib.Linux.Result and PrjFSLib.Mac.Result).

---
+++ ProjFS.Linux/Scripts/Build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+CONFIGURATION=$1
+if [ -z $CONFIGURATION ]; then
+  CONFIGURATION=Debug
+fi
+
+SCRIPTDIR=$(dirname ${BASH_SOURCE[0]})
+SRCDIR=$SCRIPTDIR/../..
+ROOTDIR=$SRCDIR/..
+PACKAGES=$ROOTDIR/packages
+
+PROJFS=$SRCDIR/ProjFS.Linux
+
+# TODO(Linux): use pkg-config to look for installed libprojfs;
+#              if not found, retrieve latest stable from repo, build
+#              into $ROOTDIR/BuildOutput/ProjFS.Linux/Native with
+#              given $CONFIGURATION
+
+# If we're building the Profiling(Release) configuration, remove Profiling() for building .NET code
+if [ "$CONFIGURATION" == "Profiling(Release)" ]; then
+  CONFIGURATION=Release
+fi
+
+dotnet restore $PROJFS/PrjFSLib.Linux.Managed/PrjFSLib.Linux.Managed.csproj /p:Configuration=$CONFIGURATION /p:Platform=x64 --packages $PACKAGES || exit 1
+dotnet build $PROJFS/PrjFSLib.Linux.Managed/PrjFSLib.Linux.Managed.csproj /p:Configuration=$CONFIGURATION /p:Platform=x64 || exit 1
