2c589f2ddca9c37c513c0b58d0cf2d71c73d1b4a
Chris Darroch <chrisd@apache.org>
2018-10-19 16:11:54 -0700

    Implement MirrorProvider support for Linux
    Add MirrorProvider support for Linux based on the Mac implementation,
    including updates to the VisualStudio solution file to add Linux build
    targets.

---
+++ MirrorProvider/Scripts/Linux/MirrorProvider_Clone.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+CONFIGURATION=$1
+if [ -z $CONFIGURATION ]; then
+  CONFIGURATION=Debug
+fi
+
+SCRIPTDIR=$(dirname ${BASH_SOURCE[0]})
+BUILDDIR=$SCRIPTDIR/../../../../BuildOutput/MirrorProvider.Linux/bin/$CONFIGURATION/x64/netcoreapp2.1
+
+dotnet $BUILDDIR/MirrorProvider.Linux.dll clone ~/PathToMirror ~/TestRoot
