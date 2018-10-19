6d6d6aaa0a88292e3ce6f4c0fd37431952b4bfae
Chris Darroch <chrisd@apache.org>
2018-10-19 15:41:26 -0700

    Managed interface for Linux projected filesystem
    Add an initial Linux implementation of a VirtualizationInstance
    with the same types, callbacks, and functions as the Mac interface.
    
    The underlying dynamic library used by the interoperability layer
    is not included here due to anticipated licensing requirements,
    but would be provided separately.
    
    Note that it may be valuable to consider a future refactoring of
    identical classes between the Linux and Mac implementations (such
    as PrjFSLib.Linux.Result and PrjFSLib.Mac.Result).

---
+++ ProjFS.Linux/PrjFSLib.Linux.Managed/CallbackDelegates.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace PrjFSLib.Linux
+{
+    // Callbacks
+    public delegate Result EnumerateDirectoryCallback(
+        ulong commandId,
+        string relativePath,
+        int triggeringProcessId,
+        string triggeringProcessName);
+
+    public delegate Result GetFileStreamCallback(
+        ulong commandId,
+        string relativePath,
+        [MarshalAs(UnmanagedType.LPArray, SizeConst = Interop.PrjFSLib.PlaceholderIdLength)]
+        byte[] providerId,
+        [MarshalAs(UnmanagedType.LPArray, SizeConst = Interop.PrjFSLib.PlaceholderIdLength)]
+        byte[] contentId,
+        int triggeringProcessId,
+        string triggeringProcessName,
+        IntPtr fileHandle);
+
+    public delegate Result NotifyOperationCallback(
+        ulong commandId,
+        string relativePath,
+        [MarshalAs(UnmanagedType.LPArray, SizeConst = Interop.PrjFSLib.PlaceholderIdLength)]
+        byte[] providerId,
+        [MarshalAs(UnmanagedType.LPArray, SizeConst = Interop.PrjFSLib.PlaceholderIdLength)]
+        byte[] contentId,
+        int triggeringProcessId,
+        string triggeringProcessName,
+        bool isDirectory,
+        NotificationType notificationType);
+
+    // Pre-event notifications
+    public delegate Result NotifyPreDeleteEvent(
+        string relativePath,
+        bool isDirectory);
+
+    public delegate Result NotifyFilePreConvertToFullEvent(
+        string relativePath);
+
+    public delegate Result NotifyPreModifyEvent(
+        string relativePath);
+
+    // Informational post-event notifications
+    public delegate void NotifyNewFileCreatedEvent(
+        string relativePath,
+        bool isDirectory);
+
+    public delegate void NotifyFileRenamedEvent(
+        string relativeDestinationPath,
+        bool isDirectory);
+
+    public delegate void NotifyHardLinkCreatedEvent(
+        string relativeNewLinkPath);
+
+    public delegate void NotifyFileModified(
+        string relativePath);
+
+    public delegate void NotifyFileDeleted(
+        string relativePath,
+        bool isDirectory);
+}
