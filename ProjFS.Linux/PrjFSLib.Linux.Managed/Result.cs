ca11c6b3edf981b3119f86173eddc8532a1cae15
Chris Darroch <chrisd@apache.org>
2018-10-19 15:41:26 -0700

    Managed interface for Linux projected filesystem
    Add an initial Linux implementation of a VirtualizationInstance
    with the same types, callbacks, and functions as the Mac interface.
    
    The underlying dynamic library used by the interoperability layer
    is not included here due to anticipated licensing requirements,
    but would be provided separately.
    
    Note that it may be valuable to consider a future refactoring of
    identical classes between the Linux and Mac implementations (such
    as PrjFSLib.Linux.Result and PrjFSLib.Mac.Result).

---
+++ ProjFS.Linux/PrjFSLib.Linux.Managed/Result.cs
@@ -0,0 +1,29 @@
+﻿namespace PrjFSLib.Linux
+{
+    public enum Result : uint
+    {
+        Invalid                             = 0x00000000,
+
+        Success                             = 0x00000001,
+        Pending                             = 0x00000002,
+
+        // Bugs in the caller
+        EInvalidArgs                        = 0x10000001,
+        EInvalidOperation                   = 0x10000002,
+        ENotSupported                       = 0x10000004,
+
+        // Runtime errors
+        EDriverNotLoaded                    = 0x20000001,
+        EOutOfMemory                        = 0x20000002,
+        EFileNotFound                       = 0x20000004,
+        EPathNotFound                       = 0x20000008,
+        EAccessDenied                       = 0x20000010,
+        EInvalidHandle                      = 0x20000020,
+        EIOError                            = 0x20000040,
+        ENotAVirtualizationRoot             = 0x20000080,
+        EVirtualizationRootAlreadyExists    = 0x20000100,
+        EDirectoryNotEmpty                  = 0x20000200,
+
+        ENotYetImplemented                  = 0xFFFFFFFF,
+    }
+}
