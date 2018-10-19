ac8812092c4574797a84752e5bbb19f2dabafbc9
Chris Darroch <chrisd@apache.org>
2018-10-19 15:41:26 -0700

    Managed interface for Linux projected filesystem
    Add an initial Linux implementation of a VirtualizationInstance
    with the same types, callbacks, and functions as the Mac interface.
    
    The underlying dynamic library used by the interoperability layer
    is not included here due to anticipated licensing requirements,
    but would be provided separately.
    
    Note that it may be valuable to consider a future refactoring of
    identical classes between the Linux and Mac implementations (such
    as PrjFSLib.Linux.Result and PrjFSLib.Mac.Result).

---
+++ ProjFS.Linux/PrjFSLib.Linux.Managed/NotificationMapping.cs
@@ -0,0 +1,8 @@
+﻿namespace PrjFSLib.Linux
+{
+    public class NotificationMapping
+    {
+        public NotificationType NotificationMask { get; set; }
+        public string NotificationRelativeRoot { get; set; }
+    }
+}
---
+++ ProjFS.Linux/PrjFSLib.Linux.Managed/UpdateType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrjFSLib.Linux
+{
+    [Flags]
+    public enum UpdateType
+    {
+        Invalid         = 0x00000000,
+
+        AllowReadOnly   = 0x00000020,
+    }
+}
