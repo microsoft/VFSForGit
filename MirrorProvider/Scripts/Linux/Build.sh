70ab8f3884370a3b8379d138eaf7fd7edc5cbbdd
Chris Darroch <chrisd@apache.org>
2018-10-19 17:07:35 -0700

    Remove Xcode profiling build config option

--- MirrorProvider/Scripts/Linux/Build.sh
+++ MirrorProvider/Scripts/Linux/Build.sh
@@ -14,11 +14,6 @@ SLN=$SRCDIR/MirrorProvider/MirrorProvider.sln
 # Build the ProjFS library interface
 $SRCDIR/ProjFS.Linux/Scripts/Build.sh $CONFIGURATION
 
-# If we're building the Profiling(Release) configuration, remove Profiling() for building .NET code
-if [ "$CONFIGURATION" == "Profiling(Release)" ]; then
-  CONFIGURATION=Release
-fi
-
 # Build the MirrorProvider
 dotnet restore $SLN /p:Configuration="$CONFIGURATION.Linux" --packages $ROOTDIR/packages
 dotnet build $SLN --configuration $CONFIGURATION.Linux
