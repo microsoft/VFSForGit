4821f22a449b029d2c4ac2cddeece5e6234059b8
John Briggs <jobriggs@microsoft.com>
2018-10-23 15:44:56 -0400

    Remove *DevService.bat scripts

--- Scripts/SetupDevService.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-CALL %~dp0\EnsureVFSDevShell.bat || EXIT /b 10
-
-IF "%1"=="" (SET "Configuration=Debug") ELSE (SET "Configuration=%1")
-
-sc create %Configuration%.GVFS.Service binPath=%VFS_OUTPUTDIR%\GVFS.Service\bin\x64\%Configuration%\GVFS.Service.exe
