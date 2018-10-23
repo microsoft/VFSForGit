f4b5868bd79305ab4a40a8b9eeead3e77c5d96be
John Briggs <jobriggs@microsoft.com>
2018-10-23 13:35:30 -0400

    Remove convenience macros from init

--- Scripts/Macros.ps1
+++
@@ -1,4 +0,0 @@
-function global:b { & (Join-Path $Env:VFS_SCRIPTSDIR "BuildGVFSForWindows.bat") Debug $args }
-function global:ftest { & (Join-Path $Env:VFS_SCRIPTSDIR "RunFunctionalTests.bat" ) Debug --full-suite "--test=$args" }
-function global:ig { & (Join-Path $Env:VFS_OUTPUTDIR "GVFS.Build\InstallG4W.bat ") }
-function global:iv { & (Join-Path $Env:VFS_OUTPUTDIR "GVFS.Build\InstallGVFS.bat ") }
--- init.cmd
+++ init.cmd
@@ -1,11 +1,5 @@
 @ECHO OFF
 
-REM Don't run twice
-IF NOT "%VFS_DEVSHELL%"=="" (
-    ECHO ERROR: This shell is already a VFS for Git developer shell.
-    EXIT /b 20
-)
-
 REM Set environment variables for interesting paths that scripts might need access to.
 PUSHD %~dp0
 SET VFS_SRCDIR=%CD%
@@ -25,24 +19,8 @@ SET VFS_PACKAGESDIR=%_PARSED_PATH_%
 CALL :RESOLVEPATH "%VFS_ENLISTMENTDIR%\.tools"
 SET VFS_TOOLSDIR=%_PARSED_PATH_%
 
-SET PATH=%VFS_TOOLSDIR%;%PATH%
-
 REM Clean up
 SET _PARSED_PATH_=
-
-REM Mark this as a dev shell and load the macros
-SET VFS_DEVSHELL=true
-TITLE VFS ^for Git Developer Shell ^(%VFS_SRCDIR%^)
-doskey /MACROFILE=.\Scripts\Macros.cmd.txt
-
-ECHO ==============================================
-ECHO * Welcome to the VFS for Git developer shell *
-ECHO *                                            *
-ECHO * Build: b                                   *
-ECHO * Install Git: ig                            *
-ECHO * Install VFS for Git: iv                    *
-ECHO * Run functional test: ftest [test name(s)]  *
-ECHO ==============================================
 
 GOTO :EOF
 
