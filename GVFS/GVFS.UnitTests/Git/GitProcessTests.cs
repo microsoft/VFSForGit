347e1f013ab73dd382bb567e201b440795567303
Derrick Stolee <dstolee@microsoft.com>
2018-10-25 11:23:41 -0400

    GitProcessTests: add simple unit test

---
+++ GVFS/GVFS.UnitTests/Git/GitProcessTests.cs
@@ -0,0 +1,18 @@
+﻿using GVFS.Common.Git;
+using GVFS.Tests.Should;
+using GVFS.UnitTests.Mock.Common;
+using NUnit.Framework;
+
+namespace GVFS.UnitTests.Git
+{
+    [TestFixture]
+    public class GitProcessTests
+    {
+        [TestCase]
+        public void TryKillRunningProcess_NeverRan()
+        {
+            GitProcess process = new GitProcess(new MockGVFSEnlistment());
+            process.TryKillRunningProcess().ShouldBeTrue();
+        }
+    }
+}
