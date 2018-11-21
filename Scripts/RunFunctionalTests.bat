e2c2fee303b35edb0f2785ce85f27bba47421d44
John Briggs <jrbriggs@gmail.com>
2018-11-20 20:43:29 -0500

    Enlistment -> drop
    PR feedback

--- Scripts/RunFunctionalTests.bat
+++ Scripts/RunFunctionalTests.bat
@@ -39,8 +39,8 @@ echo git location:
 where git
 
 :startFunctionalTests
-dotnet %VFS_OUTPUTDIR%\GVFS.FunctionalTests\bin\x64\%Configuration%\netcoreapp2.1\GVFS.FunctionalTests.dll /result:TestResultXplat.xml %2 %3 %4 %5 || goto :endFunctionalTests
-%VFS_OUTPUTDIR%\GVFS.FunctionalTests.Windows\bin\x64\%Configuration%\GVFS.FunctionalTests.Windows.exe /result:TestResultWindows.xml --windows-only %2 %3 %4 %5 || goto :endFunctionalTests
+dotnet %VFS_OUTPUTDIR%\GVFS.FunctionalTests\bin\x64\%Configuration%\netcoreapp2.1\GVFS.FunctionalTests.dll /result:TestResultNetCore.xml %2 %3 %4 %5 || goto :endFunctionalTests
+%VFS_OUTPUTDIR%\GVFS.FunctionalTests.Windows\bin\x64\%Configuration%\GVFS.FunctionalTests.Windows.exe /result:TestResultNetFramework.xml --windows-only %2 %3 %4 %5 || goto :endFunctionalTests
 
 :endFunctionalTests
 set error=%errorlevel%
