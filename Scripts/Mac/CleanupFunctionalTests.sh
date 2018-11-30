46c9ca6d139575fd64588c91fb46e34fcc844590
Nick Graczyk <nickgra@microsoft.com>
2018-11-30 12:46:09 -0800

    Clean up any processes that could be left hanging after the functional tests run as we do on Windows

--- Scripts/Mac/CleanupFunctionalTests.sh
+++ Scripts/Mac/CleanupFunctionalTests.sh
@@ -1,5 +1,10 @@
 . "$(dirname ${BASH_SOURCE[0]})/InitializeEnvironment.sh"
 
+pkill -9 -l GVFS.FunctionalTests
+pkill -9 -l git
+pkill -9 -l gvfs
+pkill -9 -l prjfs-log
+
 $VFS_SRCDIR/ProjFS.Mac/Scripts/UnloadPrjFSKext.sh
 
 sudo rm -r /GVFS.FT
