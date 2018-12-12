452c3bca897422a321fee9f9dbe41ba66cc1dc8d
Nick Graczyk🆖 <nickgra@microsoft.com>
2018-12-12 13:26:23 -0800

    Undo PlatformLoader change

--- GVFS/GVFS.PlatformLoader/PlatformLoader.Windows.cs
+++ GVFS/GVFS.PlatformLoader/PlatformLoader.Windows.cs
@@ -1,10 +1,17 @@
 using GVFS.Common;
+using GVFS.Common.Git;
 using GVFS.Platform.Windows;
+using GVFS.Virtualization.FileSystem;
 
 namespace GVFS.PlatformLoader
 {
-    public static partial class GVFSPlatformLoader
+    public static class GVFSPlatformLoader
     {
+        public static FileSystemVirtualizer CreateFileSystemVirtualizer(GVFSContext context, GVFSGitObjects gitObjects)
+        {
+            return new WindowsFileSystemVirtualizer(context, gitObjects);
+        }
+
         public static void Initialize()
         {
             GVFSPlatform.Register(new WindowsPlatform());
