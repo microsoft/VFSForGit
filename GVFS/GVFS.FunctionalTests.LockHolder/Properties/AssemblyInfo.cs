ea47d1e143254e8cf1b476363e9b3c66a6c7b4b1
Nick Graczyk <nickgra@microsoft.com>
2018-12-17 14:30:22 -0500

    Add missed CommonAssemblyVersion to Virtualization and bump copyright string to 2019.

--- GVFS/GVFS.FunctionalTests.LockHolder/Properties/AssemblyInfo.cs
+++ GVFS/GVFS.FunctionalTests.LockHolder/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("GVFS.FunctionalTests.LockHolder")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- GVFS/GVFS/Properties/AssemblyInfo.cs
+++ GVFS/GVFS/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("GVFS")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
