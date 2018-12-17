1befdef8e97d18a2e636bc423a1d27973ea0209d
Nick Graczyk <nickgra@microsoft.com>
2018-12-17 15:20:39 -0500

    Clean up all uses of hard coded versions in FunctionalTests projects and add new lines to the end of all new AssemblyInfo files.

--- GVFS/GVFS.UnitTests.Windows/Properties/AssemblyInfo.cs
+++ GVFS/GVFS.UnitTests.Windows/Properties/AssemblyInfo.cs
@@ -21,16 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("8e0d0989-21f6-4dd8-946c-39f992523cc6")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
