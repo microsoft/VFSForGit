153aa89dcea2913eebff3b00e34c11f8ebe31af2
Ameen Shah <alsha@microsoft.com>
2018-12-18 11:13:03 -0500

    Adding IProductUpgrader interface

---
+++ GVFS/GVFS.Common/IProductUpgrader.cs
@@ -0,0 +1,38 @@
+using GVFS.Common.Git;
+using System;
+
+namespace GVFS.Common
+{
+    /// <summary>
+    /// Delegate to wrap install action steps in.
+    /// This can be used to report the beginning / end of each install step.
+    /// </summary>
+    /// <param name="method">The method to run inside wrapper</param>
+    /// <param name="message">The message to display</param>
+    /// <returns></returns>
+    public delegate bool InstallActionWrapper(Func<bool> method, string message);
+
+    public interface IProductUpgrader
+    {
+        bool CanRunUsingCurrentConfig(
+            out bool isConfigError,
+            out string consoleMessage,
+            out string errorMessage);
+
+        bool Initialize(out string errorMessage);
+
+        bool TryGetNewerVersion(out Version newVersion, out string consoleMessage, out string errorMessage);
+
+        bool TryGetGitVersion(out GitVersion gitVersion, out string error);
+
+        bool TryDownloadNewestVersion(out string errorMessage);
+
+        bool TryRunInstaller(InstallActionWrapper installActionWrapper, out string error);
+
+        bool TryCleanup(out string error);
+
+        void CleanupDownloadDirectory();
+
+        bool TrySetupToolsDirectory(out string upgraderToolPath, out string error);
+    }
+}
