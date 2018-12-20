c4ecc646f804ad8781168f5c0ab969a54909d92a
Ashe Connor <ashe@kivikakk.ee>
2018-12-21 10:35:15 +1100

    use :- instead of :=

--- MirrorProvider/Scripts/Linux/MirrorProvider_Clone.sh
+++ MirrorProvider/Scripts/Linux/MirrorProvider_Clone.sh
@@ -8,7 +8,7 @@ fi
 SCRIPTDIR=$(dirname ${BASH_SOURCE[0]})
 BUILDDIR=$SCRIPTDIR/../../../../BuildOutput/MirrorProvider.Linux/bin/$CONFIGURATION/x64/netcoreapp2.1
 
-PATH_TO_MIRROR="${PATH_TO_MIRROR:=~/PathToMirror}"
-TEST_ROOT="${TEST_ROOT:=~/TestRoot}"
+PATH_TO_MIRROR="${PATH_TO_MIRROR:-~/PathToMirror}"
+TEST_ROOT="${TEST_ROOT:-~/TestRoot}"
 
 dotnet $BUILDDIR/MirrorProvider.Linux.dll clone "$PATH_TO_MIRROR" "$TEST_ROOT"
