d72d93d392f5a8d3b27a9d70f432bb85d1ef24ad
Chris Darroch <chrisd8088@github.com>
2019-01-15 15:42:45 -0800

    use $HOME instead of tilde to ensure expansion
    By moving the default ~/TestRoot and ~/PathToMirror paths
    inside quoted strings, to support overriding the defaults using
    environment variables (see commit f9fdb094113a), bash no longer
    expands the tilde characters to the value of the $HOME variable,
    so do that explicitly for the default paths.

--- MirrorProvider/Scripts/Linux/MirrorProvider_Mount.sh
+++ MirrorProvider/Scripts/Linux/MirrorProvider_Mount.sh
@@ -8,6 +8,6 @@ fi
 SCRIPTDIR=$(dirname ${BASH_SOURCE[0]})
 BUILDDIR=$SCRIPTDIR/../../../../BuildOutput/MirrorProvider.Linux/bin/$CONFIGURATION/x64/netcoreapp2.1
 
-TEST_ROOT="${TEST_ROOT:-~/TestRoot}"
+TEST_ROOT="${TEST_ROOT:-$HOME/TestRoot}"
 
 dotnet $BUILDDIR/MirrorProvider.Linux.dll mount "$TEST_ROOT"
