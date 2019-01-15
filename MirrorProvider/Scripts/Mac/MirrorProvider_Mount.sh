23723f56504caa8ab91cf0db8c6c25ee78688340
Chris Darroch <chrisd8088@github.com>
2019-01-15 15:47:08 -0800

    keep Mac and Linux MirrorProvider scripts in sync
    Bring the MirrorProvider scripts for macOS in sync with
    the changes from the Linux scripts (see commits f9fdb094113a,
    1bd487ee93ac, a22356635a50).
    
    This adds support for non-default paths to be supplied
    via environment variables when cloning or mounting the
    MirrorProvider.

--- MirrorProvider/Scripts/Mac/MirrorProvider_Mount.sh
+++ MirrorProvider/Scripts/Mac/MirrorProvider_Mount.sh
@@ -8,4 +8,6 @@ fi
 SCRIPTDIR=$(dirname ${BASH_SOURCE[0]})
 BUILDDIR=$SCRIPTDIR/../../../../BuildOutput/MirrorProvider.Mac/bin/$CONFIGURATION/x64/netcoreapp2.1
 
-dotnet $BUILDDIR/MirrorProvider.Mac.dll mount ~/TestRoot
+TEST_ROOT="${TEST_ROOT:-$HOME/TestRoot}"
+
+dotnet $BUILDDIR/MirrorProvider.Mac.dll mount "$TEST_ROOT"
