ff522001919ac19872b5647f7cc23a7e55caa46f
Phil Dennis-Jordan <phil@philjordan.eu>
2019-01-19 14:14:44 +0100

    Mac kext: explicit check for profile data size in external method
    Instead of getting IOUserClient to silently perform the check, let any size through IOUserClient and allow PerfTracing_ExportDataUserClient()'s check to kick in, which writes details to the kext log when it fails.

--- ProjFS.Mac/PrjFSKext/PrjFSLogUserClient.cpp
+++ ProjFS.Mac/PrjFSKext/PrjFSLogUserClient.cpp
@@ -19,7 +19,7 @@ static const IOExternalMethodDispatch LogUserClientDispatch[] =
             .checkScalarInputCount =    0,
             .checkStructureInputSize =  0,
             .checkScalarOutputCount =   0,
-            .checkStructureOutputSize = PrjFSPerfCounter_Count * sizeof(PrjFSPerfCounterResult), // array of results
+            .checkStructureOutputSize = kIOUCVariableStructureSize, // array of results
         },
 };
 
