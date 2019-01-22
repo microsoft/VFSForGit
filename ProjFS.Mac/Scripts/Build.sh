9659cedfe6ec50f2de6308a24a21ae7512f91e2f
Jessica Schumaker <jeschu@microsoft.com>
2019-01-22 11:54:58 -0500

    output unit tests to .xml

--- ProjFS.Mac/Scripts/Build.sh
+++ ProjFS.Mac/Scripts/Build.sh
@@ -12,7 +12,19 @@ PACKAGES=$ROOTDIR/packages
 
 PROJFS=$SRCDIR/ProjFS.Mac
 
-xcodebuild -configuration $CONFIGURATION -project $PROJFS/PrjFS.xcodeproj build -scheme 'Build All' -derivedDataPath $ROOTDIR/BuildOutput/ProjFS.Mac/Native test || exit 1
+xcodebuild -configuration $CONFIGURATION -project $PROJFS/PrjFS.xcodeproj  -scheme 'Build All' -derivedDataPath $ROOTDIR/BuildOutput/ProjFS.Mac/Native build || exit 1
+
+if !(gem list --local | grep xcpretty); then
+   sudo gem install xcpretty
+fi
+
+# Run Tests and put output into a xml file
+xcodebuild -configuration $CONFIGURATION -project $PROJFS/PrjFS.xcodeproj -scheme 'Build All' test | xcpretty -r junit --output $PROJFS/TestResultJunit.xml
+
+#Check to see if the test results file contains a failure
+if grep -q '<failure' $PROJFS/TestResultJunit.xml; then
+  exit 1
+fi
 
 # If we're building the Profiling(Release) configuration, remove Profiling() for building .NET code
 if [ "$CONFIGURATION" == "Profiling(Release)" ]; then
