aa532c105b4c25add18887073eee45046924f0be
Jameson Miller <jamill@microsoft.com>
2019-01-30 09:14:29 -0800

    Move DeleteAllInstallerDownloads into ProductUpgrader

--- GVFS/GVFS.Common/ProductUpgraderInfo.cs
+++ GVFS/GVFS.Common/ProductUpgraderInfo.cs
@@ -61,48 +61,5 @@ namespace GVFS.Common
                 Paths.GetServiceDataRoot(RootDirectory),
                 DownloadDirectory);
         }
-
-        /// <summary>
-        /// Deletes any previously downloaded installers in the Upgrader Download directory.
-        /// This can include old installers which were downloaded, but user never installed
-        /// using gvfs upgrade and GVFS is now up to date already.
-        /// </summary>
-        public static void DeleteAllInstallerDownloads(ITracer tracer = null)
-        {
-            try
-            {
-                RecursiveDelete(ProductUpgraderInfo.GetAssetDownloadsPath());
-            }
-            catch (Exception ex)
-            {
-                if (tracer != null)
-                {
-                    tracer.RelatedError($"{nameof(DeleteAllInstallerDownloads)}: Could not remove directory: {ProductUpgraderInfo.GetAssetDownloadsPath()}.{ex.ToString()}");
-                }
-            }
-        }
-
-        private static void RecursiveDelete(string path)
-        {
-            if (!Directory.Exists(path))
-            {
-                return;
-            }
-
-            DirectoryInfo directory = new DirectoryInfo(path);
-
-            foreach (FileInfo file in directory.GetFiles())
-            {
-                file.Attributes = FileAttributes.Normal;
-                file.Delete();
-            }
-
-            foreach (DirectoryInfo subDirectory in directory.GetDirectories())
-            {
-                RecursiveDelete(subDirectory.FullName);
-            }
-
-            directory.Delete();
-        }
     }
 }
