c9d9a3ae52d1f7886ac47293fe66cc71bc235a79
Chris Darroch <chrisd8088@github.com>
2019-02-20 01:10:36 -0800

    initial build of GVFS provider for Linux
    We add the core GVFS.Platform.Linux classes, derived from their
    GVFS.Platform.Mac equivalents but with appropriate changes for
    syscall argument signatures and flag values, type definitions,
    and structure fields (e.g., mode_t is a uint, and the layout of
    struct stat is quite different).
    
    In order to avoid conflicting with the GNOME Virtual FileSystem,
    we use .vfsforgit as the name of our per-repository private
    directories, instead of .gvfs, which is used on Windows and Mac.
    
    The build tooling is largely a duplicate of the corresponding
    Mac configuration, with some changes to ensure we build
    successfully on Linux (e.g., testing for "!= Windows" is no longer
    sufficient to determine that the platform is OS X, so we add
    distinct IsLinux and IsOSX build conditions to the .csproj file
    for GVFS.Upgrader, among other subprojects).
    
    The GVFS.sln configuration changes should ensure that the
    ProjFS.Linux.Managed and GVFS.Platform.Linux subprojects are
    built on all platforms (and their Mac and Windows equivalents
    are built on Linux), and that the GVFS.Service.Mac classes are
    built on Linux, so as to allow the GVFS.UnitTests subproject
    to build and run successfully on all three platforms.
    
    We also build the GVFS.Hooks.Linux and GVFS.Mount.Linux
    subprojects on both Linux and Windows, just as their Mac
    counterparts are built on both Mac and Windows.
    
    The GVFS.Service binary is, for the time being, only a stub
    on Linux, as we have yet to determine how (or if) we should
    handle auto-mounting repositories, and the GVFS.Upgrader
    binary is also non-functional on Linux at present.
    
    Note that for now the BuildGVFSForLinux.sh script does not
    perform any installation or packaging.
    
    Co-authored-by: Ashe Connor <ashe@kivikakk.ee>

--- GVFS/GVFS.Upgrader/UpgradeOrchestratorFactory.cs
+++ GVFS/GVFS.Upgrader/UpgradeOrchestratorFactory.cs
@@ -1,10 +1,14 @@
+using System;
+
 namespace GVFS.Upgrader
 {
     public static class UpgradeOrchestratorFactory
     {
         public static UpgradeOrchestrator Create(UpgradeOptions options)
         {
-#if MACOS_BUILD
+#if LINUX_BUILD
+            return new LinuxUpgradeOrchestrator(options);
+#elif MACOS_BUILD
             return new MacUpgradeOrchestrator(options);
 #elif WINDOWS_BUILD
             return new WindowsUpgradeOrchestrator(options);
