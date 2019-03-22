8a422ee17c7c402a1955ad9b281187c384d41927
Chris Darroch <chrisd8088@github.com>
2019-03-21 17:21:55 -0700

    fix chmod modes and stat fields in LinuxFileSystem
    As with commit 256334e603c8c5a689f4c6442512bd0c35395cdb, we need to
    revise the size of the mode_t type for Linux (as we ported this code
    from the Darwin/BSD version), as well as change the order and size of
    data fields in the stat(2) structure and the function signatures of
    various libc calls such as chmod() and read() to confirm to their
    Linux definitions.

--- GVFS/GVFS.Platform.Linux/LinuxFileSystem.cs
+++ GVFS/GVFS.Platform.Linux/LinuxFileSystem.cs
@@ -32,7 +32,7 @@ namespace GVFS.Platform.Linux
 
         public void ChangeMode(string path, ushort mode)
         {
-           Chmod(path, mode);
+            Chmod(path, (uint)mode);
         }
 
         public bool TryGetNormalizedPath(string path, out string normalizedPath, out string errorMessage)
@@ -63,7 +63,7 @@ namespace GVFS.Platform.Linux
         }
 
         [DllImport("libc", EntryPoint = "chmod", SetLastError = true)]
-        private static extern int Chmod(string pathname, ushort mode);
+        private static extern int Chmod(string pathname, uint mode);
 
         [DllImport("libc", EntryPoint = "rename", SetLastError = true)]
         private static extern int Rename(string oldPath, string newPath);
@@ -82,33 +82,42 @@ namespace GVFS.Platform.Linux
         private static class NativeStat
         {
             // #define  S_IFMT      0170000     /* [XSI] type of file mask */
-            private static readonly ushort IFMT = Convert.ToUInt16("170000", 8);
+            private static readonly uint IFMT = Convert.ToUInt32("170000", 8);
 
             // #define  S_IFSOCK    0140000     /* [XSI] socket */
-            private static readonly ushort IFSOCK = Convert.ToUInt16("0140000", 8);
+            private static readonly uint IFSOCK = Convert.ToUInt32("0140000", 8);
 
             // #define S_IXUSR     0000100     /* [XSI] X for owner */
-            private static readonly ushort IXUSR = Convert.ToUInt16("100", 8);
+            private static readonly uint IXUSR = Convert.ToUInt32("100", 8);
 
             // #define S_IXGRP     0000010     /* [XSI] X for group */
-            private static readonly ushort IXGRP = Convert.ToUInt16("10", 8);
+            private static readonly uint IXGRP = Convert.ToUInt32("10", 8);
 
             // #define S_IXOTH     0000001     /* [XSI] X for other */
-            private static readonly ushort IXOTH = Convert.ToUInt16("1", 8);
+            private static readonly uint IXOTH = Convert.ToUInt32("1", 8);
 
-            public static bool IsSock(ushort mode)
+            // #define _STAT_VER   1
+            private static readonly int STAT_VER = 1;
+
+            public static bool IsSock(uint mode)
             {
                 // #define  S_ISSOCK(m) (((m) & S_IFMT) == S_IFSOCK)    /* socket */
                 return (mode & IFMT) == IFSOCK;
             }
 
-            public static bool IsExecutable(ushort mode)
+            public static bool IsExecutable(uint mode)
             {
                 return (mode & (IXUSR | IXGRP | IXOTH)) != 0;
             }
 
-            [DllImport("libc", EntryPoint = "stat$INODE64", SetLastError = true)]
-            public static extern int Stat(string path, [Out] out StatBuffer statBuffer);
+            public static int Stat(string path, [Out] out StatBuffer buf)
+            {
+                return __XStat64(STAT_VER, path, out buf);
+            }
+
+            // TODO(Linux): assumes recent GNU libc or ABI-compatible libc
+            [DllImport("libc", EntryPoint = "__xstat64", SetLastError = true)]
+            public static extern int __XStat64(int vers, string path, [Out] out StatBuffer buf);
 
             [StructLayout(LayoutKind.Sequential)]
             public struct TimeSpec
@@ -117,31 +126,27 @@ namespace GVFS.Platform.Linux
                 public long Nsec;
             }
 
+            // TODO(Linux): assumes stat64 field layout of x86-64 architecture
             [StructLayout(LayoutKind.Sequential)]
             public struct StatBuffer
             {
-                public int Dev;              /* ID of device containing file */
-                public ushort Mode;          /* Mode of file (see below) */
-                public ushort NLink;         /* Number of hard links */
-                public ulong Ino;            /* File serial number */
-                public uint UID;             /* User ID of the file */
-                public uint GID;             /* Group ID of the file */
-                public int RDev;             /* Device ID */
+                public ulong Dev;           /* ID of device containing file */
+                public ulong Ino;           /* File serial number */
+                public ulong NLink;         /* Number of hard links */
+                public uint Mode;           /* Mode of file (see below) */
+                public uint UID;            /* User ID of the file */
+                public uint GID;            /* Group ID of the file */
+                public uint Padding;        /* RESERVED: DO NOT USE! */
+                public ulong RDev;          /* Device ID if special file */
+                public long Size;           /* file size, in bytes */
+                public long BlkSize;        /* optimal blocksize for I/O */
+                public long Blocks;         /* blocks allocated for file */
+                public TimeSpec ATimespec;  /* time of last access */
+                public TimeSpec MTimespec;  /* time of last data modification */
+                public TimeSpec CTimespec;  /* time of last status change */
 
-                public TimeSpec ATimespec;     /* time of last access */
-                public TimeSpec MTimespec;     /* time of last data modification */
-                public TimeSpec CTimespec;     /* time of last status change */
-                public TimeSpec BirthTimespec; /* time of file creation(birth) */
-
-                public long Size;          /* file size, in bytes */
-                public long Blocks;        /* blocks allocated for file */
-                public int BlkSize;        /* optimal blocksize for I/O */
-                public uint Glags;         /* user defined flags for file */
-                public uint Gen;           /* file generation number */
-                public int LSpare;         /* RESERVED: DO NOT USE! */
-
-                [MarshalAs(UnmanagedType.ByValArray, SizeConst = 2)]
-                public long[] QSpare;     /* RESERVED: DO NOT USE! */
+                [MarshalAs(UnmanagedType.ByValArray, SizeConst = 3)]
+                public long[] Reserved;     /* RESERVED: DO NOT USE! */
             }
         }
 
@@ -150,7 +155,7 @@ namespace GVFS.Platform.Linux
             public const int ReadOnly = 0x0000;
             public const int WriteOnly = 0x0001;
 
-            public const int Create = 0x0200;
+            public const int Create = 0x0040;
 
             public static bool TryReadFirstByteOfFile(string fileName, byte[] buffer)
             {
@@ -179,11 +184,11 @@ namespace GVFS.Platform.Linux
             public static extern int Close(int fd);
 
             [DllImport("libc", EntryPoint = "read", SetLastError = true)]
-            public static extern int Read(int fd, [Out] byte[] buf, int count);
+            public static extern long Read(int fd, [Out] byte[] buf, ulong count);
 
             private static bool TryReadOneByte(int fileDescriptor, byte[] buffer)
             {
-                int numBytes = Read(fileDescriptor, buffer, 1);
+                long numBytes = Read(fileDescriptor, buffer, 1);
 
                 if (numBytes == -1)
                 {
