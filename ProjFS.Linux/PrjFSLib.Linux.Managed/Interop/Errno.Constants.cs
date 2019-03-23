e33f9f0eddf6d8c0ee150809d844794db1b3ae67
Chris Darroch <chrisd8088@github.com>
2019-03-23 01:08:53 -0700

    remove Mono from Linux VirtualizationInstance
    Also introduce an Interop.Errno class for use with errno-to-Result
    conversions in future work.

---
+++ ProjFS.Linux/PrjFSLib.Linux.Managed/Interop/Errno.Constants.cs
@@ -0,0 +1,10 @@
+namespace PrjFSLib.Linux.Interop
+{
+    internal static partial class Errno
+    {
+        internal static class Constants
+        {
+            public const int EPERM = 1;
+        }
+    }
+}
