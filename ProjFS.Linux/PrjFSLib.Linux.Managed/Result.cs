3a1ae5a45bbb5c772a871cbf95a62066db4c3d81
Chris Darroch <chrisd8088@github.com>
2019-03-24 01:37:11 -0700

    remove Mono dependency in Linux errno conversions
    We can also migrate the conversion of errno and Result values
    into the interoperability namespace, which lets Result.cs align
    with the macOS version again.

--- ProjFS.Linux/PrjFSLib.Linux.Managed/Result.cs
+++ ProjFS.Linux/PrjFSLib.Linux.Managed/Result.cs
@@ -1,5 +1,3 @@
-using Mono.Unix.Native;
-
 namespace PrjFSLib.Linux
 {
     public enum Result : uint
@@ -26,93 +24,5 @@ namespace PrjFSLib.Linux
         EVirtualizationInvalidOperation     = 0x20000400,
 
         ENotYetImplemented                  = 0xFFFFFFFF,
-    }
-
-    internal static class ResultMethods
-    {
-        internal static int ConvertResultToErrno(this Result result)
-        {
-            switch (result)
-            {
-                case Result.Success:
-                    return 0;
-                case Result.Pending:
-                    return -(int)Errno.EINPROGRESS;
-
-                case Result.EInvalidArgs:
-                    return -(int)Errno.EINVAL;
-                case Result.EInvalidOperation:
-                case Result.EVirtualizationInvalidOperation:
-                    return -(int)Errno.EPERM;
-                case Result.ENotSupported:
-                    return -(int)Errno.EOPNOTSUPP;
-
-                case Result.EDriverNotLoaded:
-                    return -(int)Errno.ENODEV;
-                case Result.EOutOfMemory:
-                    return -(int)Errno.ENOMEM;
-                case Result.EFileNotFound:
-                case Result.EPathNotFound:
-                    return -(int)Errno.ENOENT;
-                case Result.EAccessDenied:
-                    return -(int)Errno.EPERM;
-                case Result.EInvalidHandle:
-                    return -(int)Errno.EBADF;
-                case Result.EIOError:
-                    return -(int)Errno.EIO;
-                case Result.EDirectoryNotEmpty:
-                    return -(int)Errno.ENOTEMPTY;
-
-                case Result.ENotYetImplemented:
-                    return -(int)Errno.ENOSYS;
-
-                case Result.Invalid:
-                default:
-                    return -(int)Errno.EINVAL;
-            }
-        }
-
-        internal static Result ConvertErrnoToResult(this int errno)
-        {
-            switch ((Errno)errno)
-            {
-                case 0:
-                    return Result.Success;
-
-                case Errno.EACCES:
-                case Errno.EEXIST:
-                case Errno.EPERM:
-                case Errno.EROFS:
-                    return Result.EAccessDenied;
-                case Errno.EBADF:
-                    return Result.EInvalidHandle;
-                case Errno.EDQUOT:
-                case Errno.EIO:
-                case Errno.ENODATA: // also ENOATTR; see getxattr(2)
-                case Errno.ENOSPC:
-                    return Result.EIOError;
-                case Errno.EFAULT:
-                case Errno.EINVAL:
-                case Errno.EOVERFLOW:
-                    return Result.EInvalidArgs;
-                case Errno.ELOOP:
-                case Errno.EMLINK:
-                case Errno.ENAMETOOLONG:
-                case Errno.ENOENT:
-                case Errno.ENOTDIR:
-                    return Result.EPathNotFound;
-                case Errno.ENOMEM:
-                    return Result.EOutOfMemory;
-                case Errno.ENOSYS:
-                    return Result.ENotYetImplemented;
-                case Errno.ENOTEMPTY:
-                    return Result.EDirectoryNotEmpty;
-                case Errno.EOPNOTSUPP:
-                    return Result.ENotSupported;
-
-                default:
-                    return Result.Invalid;
-            }
-        }
     }
 }
