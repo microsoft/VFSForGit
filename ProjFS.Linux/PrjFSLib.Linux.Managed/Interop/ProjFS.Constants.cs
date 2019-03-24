4fb4a6ec31bd9baf0f0c238410264d624c5683cb
Chris Darroch <chrisd8088@github.com>
2019-03-24 01:37:11 -0700

    remove Mono dependency in Linux errno conversions
    We can also migrate the conversion of errno and Result values
    into the interoperability namespace, which lets Result.cs align
    with the macOS version again.

--- ProjFS.Linux/PrjFSLib.Linux.Managed/Interop/ProjFS.Constants.cs
+++ ProjFS.Linux/PrjFSLib.Linux.Managed/Interop/ProjFS.Constants.cs
@@ -4,14 +4,14 @@ namespace PrjFSLib.Linux.Interop
     {
         internal static class Constants
         {
+            public const ulong PROJFS_ALLOW = 0x01;
+            public const ulong PROJFS_DENY = 0x02;
             public const ulong PROJFS_CLOSE_WRITE = 0x00000008;
             public const ulong PROJFS_OPEN = 0x00000020;
             public const ulong PROJFS_DELETE_SELF = 0x00000400;
             public const ulong PROJFS_MOVE_SELF = 0x00000800;
-            public const ulong PROJFS_CREATE_SELF = 0x100000000;
             public const ulong PROJFS_ONDIR = 0x40000000;
-            public const ulong PROJFS_ALLOW = 0x00000001;
-            public const ulong PROJFS_DENY = 0x00000002;
+            public const ulong PROJFS_CREATE_SELF = 0x000100000000;
         }
     }
 }
