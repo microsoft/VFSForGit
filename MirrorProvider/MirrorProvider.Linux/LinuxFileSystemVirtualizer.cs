f771beac92365411ef7a4705e3a69864ba223ac8
Chris Darroch <chrisd8088@github.com>
2019-03-29 14:45:42 -0700

    remove unsafe wrapper on MirrorProvider.Linux stat
    Matching the implementation of GVFS.Platform.Mac/MacFileSystem.cs,
    we can also avoid the unsafe keyword on our stat() DllImport calls and
    subclass in MirrorProvider.Linux.

--- MirrorProvider/MirrorProvider.Linux/LinuxFileSystemVirtualizer.cs
+++ MirrorProvider/MirrorProvider.Linux/LinuxFileSystemVirtualizer.cs
@@ -111,13 +111,7 @@ namespace MirrorProvider.Linux
                     else
                     {
                         string childRelativePath = Path.Combine(relativePath, child.Name);
-                        string childFullPathInMirror = this.GetFullPathInMirror(childRelativePath);
-                        NativeMethods.StatBuffer statBuffer = new NativeMethods.StatBuffer();
-                        int statResult;
-                        unsafe
-                        {
-                            statResult = NativeMethods.LStat(childFullPathInMirror, out statBuffer);
-                        }
+                        int statResult = NativeMethods.LStat(this.GetFullPathInMirror(childRelativePath), out NativeMethods.StatBuffer statBuffer);
                         if (statResult == -1)
                         {
                             Console.WriteLine($"NativeMethods.LStat failed: {Marshal.GetLastWin32Error()}");
@@ -283,7 +277,7 @@ namespace MirrorProvider.Linux
             return bytes;
         }
 
-        private static unsafe class NativeMethods
+        private static class NativeMethods
         {
             // #define _STAT_VER   1
             private static readonly int STAT_VER = 1;
