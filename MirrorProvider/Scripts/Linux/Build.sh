6014eb7cbb36e540040801af629024d970ce0e40
Chris Darroch <chrisd8088@github.com>
2019-03-29 15:19:24 -0700

    check libc and arch in Linux MirrorProvider build
    These checks allow us to detect compilation on systems which
    may not meet requirements introduced in
    bd6d4c8fceb4a23318ef664679f1f88b36dd7c49 for struct stat field layout
    and __lxstat64() libc ABI support.

--- MirrorProvider/Scripts/Linux/Build.sh
+++ MirrorProvider/Scripts/Linux/Build.sh
@@ -11,9 +11,27 @@ SRCDIR=$SCRIPTDIR/../../..
 ROOTDIR=$SRCDIR/..
 SLN=$SRCDIR/MirrorProvider/MirrorProvider.sln
 
+ARCH=$(uname -m)
+if test "$ARCH" != "x86_64"; then
+  >&2 echo "architecture must be x86_64 for struct stat; stopping"
+  exit 1
+fi
+
+CC=${CC:-cc}
+
+echo 'main(){int i=1;const char *n="n";struct stat b;i=__lxstat64(i,n,&b);}' | \
+  cc -xc -include sys/stat.h -o /dev/null - 2>/dev/null
+
+if test $? != 0; then
+  >&2 echo "__lxstat64() not found in libc ABI; stopping"
+  exit 1
+fi
+
 # Build the ProjFS library interface
-$SRCDIR/ProjFS.Linux/Scripts/Build.sh $CONFIGURATION
+$SRCDIR/ProjFS.Linux/Scripts/Build.sh $CONFIGURATION || exit 1
 
 # Build the MirrorProvider
 dotnet restore $SLN /p:Configuration="$CONFIGURATION.Linux" --packages $ROOTDIR/packages
 dotnet build $SLN --configuration $CONFIGURATION.Linux
+
+exit 0
