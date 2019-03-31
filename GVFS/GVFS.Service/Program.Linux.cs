79f9a5b56ac200e70f32604626dfc69cf4c7d2a1
Chris Darroch <chrisd8088@github.com>
2019-03-30 18:49:46 -0700

    add stub Linux version of GVFS.Service binary
    Based on work from commit 4b032febf1aa188683a631a4599e6dc13c88cd8d,
    from before the full GVFS.Service implementation for macOS.

---
+++ GVFS/GVFS.Service/Program.Linux.cs
@@ -0,0 +1,67 @@
+using GVFS.Common;
+using GVFS.Common.FileSystem;
+using GVFS.Common.Tracing;
+using GVFS.PlatformLoader;
+using GVFS.Service.Handlers;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace GVFS.Service
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            GVFSPlatformLoader.Initialize();
+
+            AppDomain.CurrentDomain.UnhandledException += UnhandledExceptionHandler;
+
+            using (JsonTracer tracer = new JsonTracer(GVFSConstants.Service.ServiceName, GVFSConstants.Service.ServiceName))
+            {
+                CreateService(tracer, args).Run();
+            }
+        }
+
+        private static GVFSService CreateService(JsonTracer tracer, string[] args)
+        {
+            string serviceName = args.FirstOrDefault(arg => arg.StartsWith(GVFSService.ServiceNameArgPrefix, StringComparison.Ordinal));
+            if (serviceName != null)
+            {
+                serviceName = serviceName.Substring(GVFSService.ServiceNameArgPrefix.Length);
+            }
+            else
+            {
+                serviceName = GVFSConstants.Service.ServiceName;
+            }
+
+            GVFSPlatform gvfsPlatform = GVFSPlatform.Instance;
+
+            string logFilePath = GVFSPlatform.Instance.GetLogsDirectoryForGVFSComponent(serviceName);
+            Directory.CreateDirectory(logFilePath);
+
+            tracer.AddLogFileEventListener(
+                GVFSEnlistment.GetNewGVFSLogFileName(logFilePath, GVFSConstants.LogFileTypes.Service),
+                EventLevel.Informational,
+                Keywords.Any);
+
+            string serviceDataLocation = gvfsPlatform.GetSecureDataRootForGVFSComponent(serviceName);
+            RepoRegistry repoRegistry = new RepoRegistry(
+                tracer,
+                new PhysicalFileSystem(),
+                serviceDataLocation,
+                new GVFSMountProcess(tracer),
+                new NotificationHandler(tracer));
+
+            return new GVFSService(tracer, serviceName, repoRegistry);
+        }
+
+        private static void UnhandledExceptionHandler(object sender, UnhandledExceptionEventArgs e)
+        {
+            using (JsonTracer tracer = new JsonTracer(GVFSConstants.Service.ServiceName, GVFSConstants.Service.ServiceName))
+            {
+                tracer.RelatedError($"Unhandled exception in GVFS.Service: {e.ExceptionObject.ToString()}");
+            }
+        }
+    }
+}
