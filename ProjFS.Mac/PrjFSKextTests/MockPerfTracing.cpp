737a626d2e36f96f9cc2877523591c97ba1109a1
Jessica Schumaker <jeschu@microsoft.com>
2019-04-01 13:29:04 -0400

    Fix Perf Build

--- ProjFS.Mac/PrjFSKextTests/MockPerfTracing.cpp
+++ ProjFS.Mac/PrjFSKextTests/MockPerfTracing.cpp
@@ -1,6 +1,10 @@
 #include "MockPerfTracing.hpp"
 #include <string>
 
+#if PRJFS_PERFORMANCE_TRACING_ENABLE
+uint64_t PerfTracer::s_numTracers = 0;
+#endif
+
 void PerfTracing_RecordSample(PrjFSPerfCounter counter, uint64_t startTime, uint64_t endTime)
 {
 }
