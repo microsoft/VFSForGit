9f1fc381ced33d0b8b842212049a1f3f0fc3936f
Jameson Miller <jamill@microsoft.com>
2019-04-01 10:58:43 -0400

    Remove unused using

--- GVFS/GVFS.Common/GitHubUpgrader.cs
+++ GVFS/GVFS.Common/GitHubUpgrader.cs
@@ -4,7 +4,6 @@ using GVFS.Common.Tracing;
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
