567c11b16baf42a015269e33e5c68217b6edb9f4
Ashe Connor <ashe@kivikakk.ee>
2019-04-03 15:39:29 +1100

    use "where" to locate git installation

---
+++ GVFS/GVFS.UnitTests/Platform.Mac/MacGitInstallationTests.cs
@@ -0,0 +1,18 @@
+﻿using GVFS.Platform.Mac;
+using GVFS.Tests.Should;
+using NUnit.Framework;
+
+namespace GVFS.UnitTests.Platform.Mac
+{
+    [TestFixture]
+    public class MacGitInstallationTests
+    {
+        [TestCase]
+        public void GetInstalledGitBinPathWorks()
+        {
+            string gitBinPath = new MacGitInstallation().GetInstalledGitBinPath();
+            gitBinPath.ShouldNotBeNull();
+            gitBinPath.EndsWith("/git", System.StringComparison.Ordinal).ShouldBeTrue();
+        }
+    }
+}
