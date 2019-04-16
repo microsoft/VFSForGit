385c40a732dbdee1559883c8a8cba0e6e1911181
Chris Darroch <chrisd8088@github.com>
2019-04-16 12:54:37 -0700

    remove unused notification types on Linux and Mac
    The PreModify and FileDeleted event notification types are
    only used on Windows, or not at all, so we can remove them from
    the Mac and Linux implementations.
    
    This is akin to the changes made in fc85024193ba0a06dad12f620d6518b97f008779
    to remove the NotificationMapping class on Mac and Linux.
    
    Note that we do still send PreDelete event notifications, and per the
    comments in GVFS/GVFS.Virtualization/FileSystemCallbacks.cs, the
    FileSystemVirtualizers currently expect to only receive either PreDelete
    or FileDeleted events, but not both, so removing the FileDeleted type
    should align with this expectation.

--- ProjFS.Mac/PrjFSLib.Mac.Managed/NotificationType.cs
+++ ProjFS.Mac/PrjFSLib.Mac.Managed/NotificationType.cs
@@ -15,8 +15,6 @@ namespace PrjFSLib.Mac
         HardLinkCreated     = 0x00000100,
         PreConvertToFull    = 0x00001000,
 
-        PreModify           = 0x10000001,
         FileModified        = 0x10000002,
-        FileDeleted         = 0x10000004,
     }
 }
