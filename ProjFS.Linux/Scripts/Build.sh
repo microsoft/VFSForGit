d3591f0eb895c13053ed06aee52f962e46e55fac
Chris Darroch <chrisd8088@github.com>
2019-04-23 22:21:50 -0700

    ignore Profiling(Release) build on Linux .NET Core

--- ProjFS.Linux/Scripts/Build.sh
+++ ProjFS.Linux/Scripts/Build.sh
@@ -15,6 +15,11 @@ PROJFS=$SRCDIR/ProjFS.Linux
 echo "Generating ProjFS.Linux constants..."
 "$SCRIPTDIR"/GenerateConstants.sh || exit 1
 
+# If we're building the Profiling(Release) configuration, remove Profiling() for building .NET code
+if [ "$CONFIGURATION" == "Profiling(Release)" ]; then
+  CONFIGURATION=Release
+fi
+
 echo "Restoring and building ProjFS.Linux packages..."
 dotnet restore $PROJFS/PrjFSLib.Linux.Managed/PrjFSLib.Linux.Managed.csproj /p:Configuration=$CONFIGURATION /p:Platform=x64 --packages $PACKAGES || exit 1
 dotnet build $PROJFS/PrjFSLib.Linux.Managed/PrjFSLib.Linux.Managed.csproj /p:Configuration=$CONFIGURATION /p:Platform=x64 || exit 1
