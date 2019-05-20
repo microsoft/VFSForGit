e29ec9947474370c0ef012827fd5d45083a9cbf7
Ashe Connor <kivikakk@github.com>
2019-05-20 11:07:25 +1000

    refactor ReadLink into MirrorProvider.POSIX

---
+++ MirrorProvider/MirrorProvider.POSIX/POSIXNative.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+
+namespace MirrorProvider.POSIX
+{
+    public static class POSIXNative
+    {
+        public static string ReadLink(string path)
+        {
+            const ulong BufSize = 4096;
+            byte[] targetBuffer = new byte[BufSize];
+            long bytesRead = __ReadLink(path, targetBuffer, BufSize);
+            if (bytesRead < 0)
+            {
+                return null;
+            }
+
+            targetBuffer[bytesRead] = 0;
+            return Encoding.UTF8.GetString(targetBuffer);
+        }
+
+        [DllImport("libc", EntryPoint = "readlink", SetLastError = true)]
+        public static extern long __ReadLink(
+            string path,
+            byte[] buf,
+            ulong bufsize);
+    }
+}
