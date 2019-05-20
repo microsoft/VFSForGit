a6205f5fdcb1bd5656295fd076b7fda522130fc6
Ashe Connor <ashe@kivikakk.ee>
2019-05-21 09:49:56 +1000

    avoid crash on 4096 byte link target

---
+++ MirrorProvider/MirrorProvider.Linux/LinuxNative.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MirrorProvider.POSIX;
+
+namespace MirrorProvider.Linux
+{
+    public static class LinuxNative
+    {
+        public const int ENAMETOOLONG = 36;
+
+        public static string ReadLink(string path, out int error)
+        {
+            return POSIXNative.ReadLink(path, ENAMETOOLONG, out error);
+        }
+    }
+}
