3c813eae3856dae4a35caffdf4e6853551f697f9
Ashe Connor <ashe@kivikakk.ee>
2019-06-06 11:07:22 +1000

    Simplify excluded categories in functional tests

--- GVFS/GVFS.FunctionalTests/Program.cs
+++ GVFS/GVFS.FunctionalTests/Program.cs
@@ -101,15 +101,6 @@ namespace GVFS.FunctionalTests
                 includeCategories.Remove(Categories.ExtraCoverage);
             }
 
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-            {
-                excludeCategories.Add(Categories.WindowsOnly);
-            }
-            else
-            {
-                excludeCategories.Add(Categories.LinuxOnly);
-            }
-
             if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
             {
                 excludeCategories.Add(Categories.MacTODO.NeedsNewFolderCreateNotification);
@@ -119,9 +110,17 @@ namespace GVFS.FunctionalTests
                 excludeCategories.Add(Categories.MacTODO.NeedsStatusCache);
                 excludeCategories.Add(Categories.MacTODO.TestNeedsToLockFile);
                 excludeCategories.Add(Categories.WindowsOnly);
+                excludeCategories.Add(Categories.LinuxOnly);
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                excludeCategories.Add(Categories.WindowsOnly);
+                excludeCategories.Add(Categories.MacOnly);
             }
             else
             {
+                // Windows excludes.
+                excludeCategories.Add(Categories.LinuxOnly);
                 excludeCategories.Add(Categories.MacOnly);
             }
 
