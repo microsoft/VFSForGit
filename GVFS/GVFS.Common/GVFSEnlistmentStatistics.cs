1ac109c5956f14cec6275ed7517ad6e50cfb6951
Ben Floyd <t-befloy@microsoft.com>
2019-06-11 12:02:49 -0400

    Refactored business logic, improved readability
    Pulled all of the business logic to GVFS/Common/GVFSEnlistmentStatistics
    Instance based object that stores the data and then uses it to compute
    the statistics as the statistics verb needs it. Fixed the check for if
    an entry in the database is a file or a directory, no file system calls
    or regex assumptions necessary. Also added the feature of selecting to
    run statistics on a directory with the `-d` or `--directory` option.
    `-d .` will point it at the current working directory and only report on
    files underneath the specified location. Next commit should include test
    cases.

---
+++ GVFS/GVFS.Common/GVFSEnlistmentStatistics.cs
@@ -0,0 +1,175 @@
+﻿using GVFS.Common.Database;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace GVFS.Common
+{
+    public class GVFSEnlistmentStatistics
+    {
+        private List<string> gitPaths;
+        private List<string> placeholderFolderPaths;
+        private List<string> placeholderFilePaths;
+        private List<string> modifiedFolderPaths;
+        private List<string> modifiedFilePaths;
+        private int trackedFilesCount;
+        private int placeholderCount;
+        private int modifiedPathsCount;
+        private Dictionary<string, int> trackedFilesDirectoryTracking;
+        private Dictionary<string, int> hydratedFilesDirectoryTracking;
+
+        public GVFSEnlistmentStatistics(List<string> gitPaths, List<string> placeholderFolderPaths, List<string> placeholderFilePaths, List<string> modifiedFolderPaths, List<string> modifiedFilePaths)
+        {
+            this.gitPaths = gitPaths;
+            this.placeholderFolderPaths = placeholderFolderPaths;
+            this.placeholderFilePaths = placeholderFilePaths;
+            this.modifiedFolderPaths = modifiedFolderPaths;
+            this.modifiedFilePaths = modifiedFilePaths;
+        }
+
+        public void CalculateStatistics(string parentDirectory)
+        {
+            this.trackedFilesCount = 0;
+            this.placeholderCount = 0;
+            this.modifiedPathsCount = 0;
+            this.trackedFilesDirectoryTracking = new Dictionary<string, int>();
+            this.hydratedFilesDirectoryTracking = new Dictionary<string, int>();
+
+            this.CategorizePaths(this.gitPaths, this.trackedFilesDirectoryTracking, ref this.trackedFilesCount, parentDirectory, isFile: true);
+            this.CategorizePaths(this.placeholderFolderPaths, this.hydratedFilesDirectoryTracking, ref this.placeholderCount, parentDirectory, isFile: false);
+            this.CategorizePaths(this.placeholderFilePaths, this.hydratedFilesDirectoryTracking, ref this.placeholderCount, parentDirectory, isFile: true);
+            this.CategorizePaths(this.modifiedFolderPaths, this.hydratedFilesDirectoryTracking, ref this.modifiedPathsCount, parentDirectory, isFile: false);
+            this.CategorizePaths(this.modifiedFilePaths, this.hydratedFilesDirectoryTracking, ref this.modifiedPathsCount, parentDirectory, isFile: true);
+        }
+
+        public List<string> GetDirectoriesSortedByHydration()
+        {
+            List<string> directoriesSortedByHydration = new List<string>();
+
+            foreach (KeyValuePair<string, int> keyValuePair in this.trackedFilesDirectoryTracking)
+            {
+                directoriesSortedByHydration.Add(keyValuePair.Key);
+            }
+
+            directoriesSortedByHydration.Sort(
+                delegate(string directoryOne, string directoryTwo)
+                {
+                    return this.GetHydrationOfDirectory(directoryOne).CompareTo(this.GetHydrationOfDirectory(directoryTwo)) * -1; // Multiply by -1 to reverse the sort
+                });
+
+            return directoriesSortedByHydration;
+        }
+
+        public double GetHydrationOfDirectory(string directory)
+        {
+            if (!this.hydratedFilesDirectoryTracking.ContainsKey(directory))
+            {
+                return 0;
+            }
+
+            return (double)this.hydratedFilesDirectoryTracking[directory] / this.trackedFilesDirectoryTracking[directory];
+        }
+
+        public int GetTrackedFilesCount()
+        {
+            return this.trackedFilesCount;
+        }
+
+        public int GetPlaceholderCount()
+        {
+            return this.placeholderCount;
+        }
+
+        public int GetModifiedPathsCount()
+        {
+            return this.modifiedPathsCount;
+        }
+
+        public double GetPlaceholderPercentage()
+        {
+            return (double)this.placeholderCount / this.trackedFilesCount;
+        }
+
+        public double GetModifiedPathsPercentage()
+        {
+            return (double)this.modifiedPathsCount / this.trackedFilesCount;
+        }
+
+        /// <summary>
+        /// Take a string representing a file path on system and pull out the upper-most directory from it as a string, or '/' if it is in the root
+        /// </summary>
+        /// <param name="path">The path to a file to parse for the top level directory containing it</param>
+        /// <returns>A string containing the top level directory from the provided path, or '/' if the path is for an item in the root</returns>
+        private string ParseTopDirectory(string path)
+        {
+            int whackLocation = path.IndexOf('/');
+            if (whackLocation == -1)
+            {
+                return "/";
+            }
+
+            return path.Substring(0, whackLocation);
+        }
+
+        /// <summary>
+        /// Categorizes a list of paths given as strings by mapping them to the top level directory in their path
+        /// Modifies the directoryTracking dictionary to have an accurate count of the files underneath a top level directory
+        /// </summary>
+        /// <remarks>
+        /// The distinction between files and directories is important --
+        /// If the path to a file doesn't contain a '/', then that means it falls within the root
+        /// However if a directory's path doesn't contain a '/', it counts towards itself for hydration
+        /// </remarks>
+        /// <param name="paths">An enumerable containing paths as strings</param>
+        /// <param name="directoryTracking">A dictionary used to track the number of files per top level directory</param>
+        private void CategorizePaths(IEnumerable<string> paths, Dictionary<string, int> directoryTracking, ref int count, string parentDirectory, bool isFile)
+        {
+            foreach (string path in paths)
+            {
+                // Ensure all paths have the same slashes
+                string formattedPath = path.Replace('\\', '/');
+
+                if (formattedPath.StartsWith(parentDirectory))
+                {
+                    count++;
+
+                    if (isFile)
+                    {
+                        string topDir = this.ParseTopDirectory(this.TrimDirectoryTarget(formattedPath, parentDirectory));
+                        directoryTracking[topDir] = directoryTracking.ContainsKey(topDir) ? directoryTracking[topDir] + 1 : 1;
+                    }
+                    else
+                    {
+                        if (!parentDirectory.Equals(formattedPath))
+                        {
+                            string topDir = this.TrimDirectoryTarget(formattedPath, parentDirectory);
+                            if (topDir.IndexOf('/') != -1)
+                            {
+                                topDir = this.ParseTopDirectory(topDir);
+                            }
+
+                            directoryTracking[topDir] = directoryTracking.ContainsKey(topDir) ? directoryTracking[topDir] + 1 : 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Format a given path by trimming off the leading part containing the target directory
+        /// </summary>
+        /// <param name="toTrim">The path to trim the original directory off of</param>
+        /// <returns>The newly formatted path without that initial directory</returns>
+        private string TrimDirectoryTarget(string path, string directoryTarget)
+        {
+            if (directoryTarget.Length == 0)
+            {
+                return path;
+            }
+            else
+            {
+                return path.Substring(directoryTarget.Length + 1);
+            }
+        }
+    }
+}
--- GVFS/GVFS/CommandLine/StatisticsVerb.cs
+++ GVFS/GVFS/CommandLine/StatisticsVerb.cs
@@ -19,11 +19,11 @@ namespace GVFS.CommandLine
         private const int DirectoryDisplayCount = 5;
 
         [Option(
-            'c',
-            "crawl",
+            'd',
+            "directory",
             Required = false,
-            HelpText = "Hydrate all files (for testing purposes)")]
-        public bool ToCrawl { get; set; }
+            HelpText = "Run the statistics tool on a specific directory")]
+        public string Directory { get; set; }
 
         protected override string VerbName
         {
@@ -32,81 +32,88 @@ namespace GVFS.CommandLine
 
         protected override void Execute(GVFSEnlistment enlistment)
         {
-            // Debugging entry point to force the creation of placeholders
-            if (this.ToCrawl)
+            // The path to the root of git's tree
+            string sourceRoot = enlistment.DotGitRoot.Substring(0, enlistment.DotGitRoot.Length - 4);
+
+            if (this.Directory == null)
             {
-                this.Crawl();
+                this.Directory = string.Empty;
             }
+            else if (this.Directory.Equals("."))
+            {
+                this.Directory = Environment.CurrentDirectory.Substring(sourceRoot.Length);
+            }
+
+            // Get all of the data needed to calculate statistics then pass them into GVFSEnlistmentStatistics
+            List<string> modifiedPathsFileList = new List<string>();
+            List<string> modifiedPathsFolderList = new List<string>();
+            List<string> placeholderFilePathList = new List<string>();
+            List<string> placeholderFolderPathList = new List<string>();
 
             this.GetPlaceholdersFromDatabase(enlistment, out List<IPlaceholderData> filePlaceholders, out List<IPlaceholderData> folderPlaceholders);
-            string[] modifiedPathsList = this.GetModifiedPathsFromPipe(enlistment);
-
-            /* Failed code for creating a local instance of the modified paths database
-            using (JsonTracer tracer = new JsonTracer(GVFSConstants.GVFSEtwProviderName, "Statistics"))
+            foreach (IPlaceholderData placeholderData in filePlaceholders)
             {
-                ModifiedPathsDatabase.TryLoadOrCreate(
-                    tracer,
-                    Path.Combine(enlistment.DotGVFSRoot, GVFSConstants.DotGVFS.Databases.ModifiedPaths),
-                    new PhysicalFileSystem(),
-                    out ModifiedPathsDatabase modifiedPathsDatabase,
-                    out string err);
+                placeholderFilePathList.Add(placeholderData.Path);
             }
-            */
 
-            int placeholderCount = filePlaceholders.Count + folderPlaceholders.Count;
-            int modifiedPathsCount = modifiedPathsList.Length;
+            foreach (IPlaceholderData placeholderData in folderPlaceholders)
+            {
+                placeholderFolderPathList.Add(placeholderData.Path);
+            }
 
-            Dictionary<string, int> trackedFilesLookup = this.ParseGitIndex(enlistment, out int trackedFilesCount);
-            Dictionary<string, int> topLevelDirectoryHydrationLookup = new Dictionary<string, int>();
+            foreach (string path in this.GetModifiedPathsFromPipe(enlistment))
+            {
+                if (path.Last() == '/')
+                {
+                    modifiedPathsFolderList.Add(path.Take(path.Length - 1).ToString());
+                }
+                else
+                {
+                    modifiedPathsFileList.Add(path);
+                }
+            }
 
-            // Update the dictionaries
-            this.CategorizePathsString(modifiedPathsList, topLevelDirectoryHydrationLookup);
-            this.CategorizePathsIPlaceholderData(filePlaceholders, topLevelDirectoryHydrationLookup);
-            this.CategorizePathsIPlaceholderData(folderPlaceholders, topLevelDirectoryHydrationLookup);
+            List<string> gitPathsList = this.GetPathsFromGitIndex(enlistment);
 
-            string trackedFilesCountFormatted = trackedFilesCount.ToString("N0");
-            string placeholderCountFormatted = placeholderCount.ToString("N0");
-            string modifiedPathsCountFormatted = modifiedPathsCount.ToString("N0");
+            GVFSEnlistmentStatistics enlistmentStatistics = new GVFSEnlistmentStatistics(gitPathsList, placeholderFolderPathList, placeholderFilePathList, modifiedPathsFolderList, modifiedPathsFileList);
+            enlistmentStatistics.CalculateStatistics(this.Directory);
+
+            string trackedFilesCountFormatted = enlistmentStatistics.GetTrackedFilesCount().ToString("N0");
+            string placeholderCountFormatted = enlistmentStatistics.GetPlaceholderCount().ToString("N0");
+            string modifiedPathsCountFormatted = enlistmentStatistics.GetModifiedPathsCount().ToString("N0");
 
             // Calculate spacing for the numbers of total files
             int longest = Math.Max(trackedFilesCountFormatted.Length, placeholderCountFormatted.Length);
             longest = Math.Max(longest, modifiedPathsCountFormatted.Length);
 
             // Sort the dictionary to find the most hydrated directories by percentage
-            List<KeyValuePair<string, int>> topLevelDirectoriesByHydration = topLevelDirectoryHydrationLookup.ToList();
-            topLevelDirectoriesByHydration.Sort(
-                delegate(KeyValuePair<string, int> pairOne, KeyValuePair<string, int> pairTwo)
-                {
-                    return ((double)pairOne.Value / trackedFilesLookup[pairOne.Key]).CompareTo((double)pairTwo.Value / trackedFilesLookup[pairTwo.Key]) * -1; // Multiply by -1 to reverse the sort
-                });
+            List<string> topLevelDirectoriesByHydration = enlistmentStatistics.GetDirectoriesSortedByHydration();
 
             this.Output.WriteLine("\nRepository statistics");
             this.Output.WriteLine("Total paths tracked by git:     " + trackedFilesCountFormatted.PadLeft(longest) + " | 100%");
-            this.Output.WriteLine("Total number of placeholders:   " + placeholderCountFormatted.PadLeft(longest) + " | " + this.FormatPercent(((double)placeholderCount) / trackedFilesCount));
-            this.Output.WriteLine("Total number of modified paths: " + modifiedPathsCountFormatted.PadLeft(longest) + " | " + this.FormatPercent(((double)modifiedPathsCount) / trackedFilesCount));
+            this.Output.WriteLine("Total number of placeholders:   " + placeholderCountFormatted.PadLeft(longest) + " | " + this.FormatPercent(enlistmentStatistics.GetPlaceholderPercentage()));
+            this.Output.WriteLine("Total number of modified paths: " + modifiedPathsCountFormatted.PadLeft(longest) + " | " + this.FormatPercent(enlistmentStatistics.GetModifiedPathsPercentage()));
 
-            this.Output.WriteLine("\nTotal hydration percentage:     " + this.FormatPercent((double)(placeholderCount + modifiedPathsCount) / trackedFilesCount).PadLeft(longest + 7));
+            this.Output.WriteLine("\nTotal hydration percentage:     " + this.FormatPercent(enlistmentStatistics.GetPlaceholderPercentage() + enlistmentStatistics.GetModifiedPathsPercentage()).PadLeft(longest + 7));
 
             this.Output.WriteLine("\nMost hydrated top level directories:");
 
             int maxDirectoryNameLength = 0;
             for (int i = 0; i < 5 && i < topLevelDirectoriesByHydration.Count; i++)
             {
-                maxDirectoryNameLength = Math.Max(maxDirectoryNameLength, topLevelDirectoriesByHydration[i].Key.Length);
+                maxDirectoryNameLength = Math.Max(maxDirectoryNameLength, topLevelDirectoriesByHydration[i].Length);
             }
 
             for (int i = 0; i < DirectoryDisplayCount && i < topLevelDirectoriesByHydration.Count; i++)
             {
-                string dir = topLevelDirectoriesByHydration[i].Key.PadRight(maxDirectoryNameLength);
-                string percent = this.FormatPercent((double)topLevelDirectoriesByHydration[i].Value / trackedFilesLookup[topLevelDirectoriesByHydration[i].Key]);
+                string dir = topLevelDirectoriesByHydration[i].PadRight(maxDirectoryNameLength);
+                string percent = this.FormatPercent(enlistmentStatistics.GetHydrationOfDirectory(topLevelDirectoriesByHydration[i]));
                 this.Output.WriteLine(" " + percent + " | " + dir);
             }
 
-            bool healthyRepo = (placeholderCount + modifiedPathsCount) < (trackedFilesCount / 2);
+            bool healthyRepo = (enlistmentStatistics.GetPlaceholderPercentage() + enlistmentStatistics.GetModifiedPathsPercentage()) < (0.5);
 
             this.Output.WriteLine("\nRepository status: " + (healthyRepo ? "Healthy" : "Unhealthy"));
-
-            // Console.ReadLine();
         }
 
         /// <summary>
@@ -127,34 +134,6 @@ namespace GVFS.CommandLine
         private string TrimGitIndexLine(string line)
         {
             return line.Substring(line.IndexOf("blob") + 46);
-        }
-
-        /// <summary>
-        /// Take a string representing a file path on system and pull out the upper-most directory from it as a string, or '/' if it is in the root
-        /// </summary>
-        /// <param name="path">The path to a file to parse for the top level directory containing it</param>
-        /// <returns>A string containing the top level directory from the provided path, or '/' if the path is for an item in the root</returns>
-        private string ParseTopDirectory(string path)
-        {
-            int whackLocation = path.IndexOf('/');
-            if (whackLocation == -1)
-            {
-                return "/";
-            }
-
-            return path.Substring(0, whackLocation);
-        }
-
-        /// <summary>
-        /// Temporary function for intentionally creating additional placeholders for debugging purposes
-        /// </summary>
-        private void Crawl()
-        {
-            DirectoryInfo dirInfo = new DirectoryInfo("C:/repos/VFSForGit.git/");
-            dirInfo.EnumerateDirectories()
-                .AsParallel()
-                .SelectMany(di => di.EnumerateFiles("*.*", SearchOption.AllDirectories))
-                .Count();
         }
 
         /// <summary>
@@ -212,87 +191,19 @@ namespace GVFS.CommandLine
             placeholderTable.GetAllEntries(out filePlaceholders, out folderPlaceholders);
         }
 
-        /// <summary>
-        /// Use the git process class to list the tree at the head to count and classify the files being tracked by git
-        /// Returns a dictionary mapping the top level directory to the number of files tracked inside of them
-        /// </summary>
-        /// <param name="enlistment">The current GVFS enlistment being operated on</param>
-        /// <param name="totalTrackedFilesCount">Out parameter giving back the total number of files being tracked by git</param>
-        /// <returns></returns>
-        private Dictionary<string, int> ParseGitIndex(GVFSEnlistment enlistment, out int totalTrackedFilesCount)
+        private List<string> GetPathsFromGitIndex(GVFSEnlistment enlistment)
         {
-            Dictionary<string, int> trackedFilesLookup = new Dictionary<string, int>();
-            int addedFiles = 0;
-
+            List<string> gitPaths = new List<string>();
             GitProcess gitProcess = new GitProcess(enlistment);
+
             GitProcess.Result result = gitProcess.LsTree(
                 GVFSConstants.DotGit.HeadName,
                 line =>
                 {
-                    string topDir = this.ParseTopDirectory(this.TrimGitIndexLine(line));
-                    trackedFilesLookup[topDir] = trackedFilesLookup.ContainsKey(topDir) ? trackedFilesLookup[topDir] + 1 : 1;
-                    ++addedFiles;
+                    gitPaths.Add(this.TrimGitIndexLine(line));
                 },
                 recursive: true);
-
-            totalTrackedFilesCount = addedFiles;
-            return trackedFilesLookup;
-        }
-
-        /// <summary>
-        /// Categorizes a list of paths given as strings by mapping them to the top level directory in their path
-        /// Modifies the directoryTracking dictionary to have an accurate count of the files underneath a top level directory
-        /// </summary>
-        /// <remarks>
-        /// The distinction between files and directories is important --
-        /// If the path to a file doesn't contain a '/', then that means it falls within the root
-        /// However if a directory's path doesn't contain a '/', it counts towards itself for hydration
-        /// </remarks>
-        /// <param name="paths">An enumerable containing paths as strings</param>
-        /// <param name="directoryTracking">A dictionary used to track the number of files per top level directory</param>
-        private void CategorizePathsString(IEnumerable<string> paths, Dictionary<string, int> directoryTracking)
-        {
-            foreach (string path in paths)
-            {
-                // Ensure all paths have the same slashes
-                string formattedPath = path.Replace('\\', '/');
-
-                // Making the assumption that all paths to files have the '.' character and that all paths to directories do not
-                bool isFile = formattedPath.IndexOf('.') != -1;
-
-                if (isFile)
-                {
-                    string topDir = this.ParseTopDirectory(formattedPath);
-                    directoryTracking[topDir] = directoryTracking.ContainsKey(topDir) ? directoryTracking[topDir] + 1 : 1;
-                }
-                else
-                {
-                    string topDir = formattedPath;
-                    if (topDir.IndexOf('/') != -1)
-                    {
-                        topDir = this.ParseTopDirectory(topDir);
-                    }
-
-                    directoryTracking[topDir] = directoryTracking.ContainsKey(topDir) ? directoryTracking[topDir] + 1 : 1;
-                }
-            }
-        }
-
-        /// <summary>
-        /// Wrapper method to call <see cref="StatisticsVerb.CategorizePathsString(IEnumerable{string}, Dictionary{string, int})"/> on an enumerable of IPlaceholderData objects
-        /// For example: <see cref="StatisticsVerb.GetPlaceholdersFromDatabase(GVFSEnlistment, out List{IPlaceholderData}, out List{IPlaceholderData})"/>
-        /// </summary>
-        /// <param name="placeholderData">An enumerable list of IPlaceholderData objects which each internally have a path</param>
-        /// <param name="directoryTracking">A dictionary used to track the number of files per top level directory</param>
-        private void CategorizePathsIPlaceholderData(IEnumerable<IPlaceholderData> placeholderData, Dictionary<string, int> directoryTracking)
-        {
-            string[] stringPaths = new string[placeholderData.Count()];
-            for (int i = 0; i < stringPaths.Length; i++)
-            {
-                stringPaths[i] = placeholderData.ElementAt(i).Path;
-            }
-
-            this.CategorizePathsString(stringPaths, directoryTracking);
+            return gitPaths;
         }
     }
 }
