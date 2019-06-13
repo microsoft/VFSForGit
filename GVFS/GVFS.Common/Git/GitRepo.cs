03ff1709bdd3e22180dc0e64ef2310e8c2e2bbb9
Chris Darroch <chrisd8088@github.com>
2019-06-13 12:24:48 -0700

    always lowercase SHA1 filenames for Linux
    On Linux, file paths are case-sensitive, so we always use
    lowercase paths generated from SHA1 values because that
    ensures consistency with Git's own naming scheme for
    files under .git/objects.

--- GVFS/GVFS.Common/Git/GitRepo.cs
+++ GVFS/GVFS.Common/Git/GitRepo.cs
@@ -224,6 +224,8 @@ namespace GVFS.Common.Git
 
         private LooseBlobState GetLooseBlobState(string blobSha, Action<Stream, long> writeAction, out long size)
         {
+            // Ensure SHA path is lowercase for Linux
+            blobSha = blobSha.ToLower();
             string blobPath = Path.Combine(
                 this.enlistment.GitObjectsRoot,
                 blobSha.Substring(0, 2),
