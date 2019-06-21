7171d83acb55f66715eb534cee73708a737de040
Kevin Willford <kewillf@microsoft.com>
2019-06-21 17:09:24 -0600

    Update projection to use the included folders

---
+++ GVFS/GVFS.Virtualization/Projection/GitIndexProjection.IncludedFolder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GVFS.Virtualization.Projection
+{
+    public partial class GitIndexProjection
+    {
+        internal class IncludedFolderData
+        {
+            public IncludedFolderData()
+            {
+                this.Children = new Dictionary<string, IncludedFolderData>();
+            }
+
+            public bool IsRecursive { get; set; }
+            public Dictionary<string, IncludedFolderData> Children { get; }
+        }
+    }
+}
