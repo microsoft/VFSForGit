13e2cf3630c7db20b9cdf6ebbb54bd4d2056ae73
Tomas Urbonaitis <tomasurb@uber.com>
2019-07-10 18:32:53 +0300

    Adding manpage link to comment

--- GVFS/GVFS.Platform.Mac/MacPlatform.cs
+++ GVFS/GVFS.Platform.Mac/MacPlatform.cs
@@ -168,6 +168,7 @@ namespace GVFS.Platform.Mac
                 get { return "vfsforgit"; }
             }
 
+            // Documented here (in the addressing section): https://www.unix.com/man-page/mojave/4/unix/
             public override int MaxPipePathLength => 104;
         }
     }
