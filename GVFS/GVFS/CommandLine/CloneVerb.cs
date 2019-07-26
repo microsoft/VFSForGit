05907ce6b7e02e9bb6b12ea3fc421ce0f99332a5
William Baker <wilbaker@microsoft.com>
2019-07-26 09:05:43 -0700

    Update comment for CreateGitScript

--- GVFS/GVFS/CommandLine/CloneVerb.cs
+++ GVFS/GVFS/CommandLine/CloneVerb.cs
@@ -664,7 +664,7 @@ namespace GVFS.CommandLine
             return new Result(true);
         }
 
-        // TODO(#1364): either adjust to "git" or remove entirely
+        // TODO(#1364): Don't call this method on POSIX platforms (or have it no-op on them)
         private void CreateGitScript(GVFSEnlistment enlistment)
         {
             FileInfo gitCmd = new FileInfo(Path.Combine(enlistment.EnlistmentRoot, "git.cmd"));
