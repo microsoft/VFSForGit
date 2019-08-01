9c5503c61ed64e3c4e1eec0d557d7b1f17485e2b
Chris Darroch <chrisd8088@github.com>
2019-08-01 13:59:33 -0700

    auto download and install VFSForGit Git on Linux
    These scripts are very similar to the Mac versions, but instead
    download the gitforlinux.gvfs.installer NuGet package and use
    the .deb package it contains to install the VFSForGet-enabled
    Git client on Linux.
    
    We also add a PrepFunctionalTests.sh script to use with the
    functional test suite.

--- Scripts/Linux/DownloadGVFSGit.sh
+++ Scripts/Linux/DownloadGVFSGit.sh
@@ -4,4 +4,4 @@ BUILDDIR=$VFS_OUTPUTDIR/GVFS.Build
 GITVERSION="$($VFS_SCRIPTDIR/GetGitVersionNumber.sh)"
 cp $VFS_SRCDIR/nuget.config $BUILDDIR
 dotnet new classlib -n Restore.GitInstaller -o $BUILDDIR --force
-dotnet add $BUILDDIR/Restore.GitInstaller.csproj package --package-directory $VFS_PACKAGESDIR GitForMac.GVFS.Installer --version $GITVERSION
+dotnet add $BUILDDIR/Restore.GitInstaller.csproj package --package-directory $VFS_PACKAGESDIR GitForLinux.GVFS.Installer --version $GITVERSION
