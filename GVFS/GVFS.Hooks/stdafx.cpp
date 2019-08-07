0cfa0f45cb71266ccd157b627b0394e90f551899
William Baker <wilbaker@microsoft.com>
2019-08-07 16:09:39 -0700

    CLEANUP: Fix GVFS.Hooks Version.rc and stdafx.cpp files

--- GVFS/GVFS.Hooks/stdafx.cpp
+++ GVFS/GVFS.Hooks/stdafx.cpp
@@ -1,5 +1,5 @@
 // stdafx.cpp : source file that includes just the standard includes
-// GVFS.ReadObjectHook.pch will be the pre-compiled header
+// GVFS.Hooks.pch will be the pre-compiled header
 // stdafx.obj will contain the pre-compiled type information
 
 #include "stdafx.h"
