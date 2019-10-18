61f30ff29772f9cc16b628e23371da84128d6397
Kevin Willford <Kevin.Willford@microsoft.com>
2019-10-18 10:06:43 -0600

    Do not try to uninstall prjflt unless flag is passed

--- Scripts/UninstallGVFS.bat
+++ Scripts/UninstallGVFS.bat
@@ -9,11 +9,13 @@ if not exist "c:\Program Files\GVFS" goto :end
 
 call %VFS_SCRIPTSDIR%\StopAllServices.bat
 
+if not "%2"=="--remove-prjflt" goto :uninstallGVFS
 REM The GVFS uninstaller will not remove prjflt, and so we must remove it ourselves first.  If we don't, the non-inbox ProjFS
 REM will cause problems next time the inbox ProjFS is enabled
 call %VFS_SCRIPTSDIR%\StopService.bat prjflt
 rundll32.exe SETUPAPI.DLL,InstallHinfSection DefaultUninstall 128 C:\Program Files\GVFS\Filter\prjflt.inf
 
+:uninstallGVFS
 REM Find the latest uninstaller file by date and run it. Goto the next step after a single execution.
 for /F "delims=" %%f in ('dir "c:\Program Files\GVFS\unins*.exe" /B /S /O:-D') do %%f /VERYSILENT /SUPPRESSMSGBOXES /NORESTART & goto :deleteGVFS
 
