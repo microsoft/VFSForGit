8470547cc94d48b1762ce30de651f5808fdb67cb
Chris Darroch <chrisd8088@github.com>
2020-01-05 19:12:53 -0800

    [WIP] expand sparse test to check deleted files

--- GVFS/GVFS.FunctionalTests/Tests/EnlistmentPerFixture/SparseTests.cs
+++ GVFS/GVFS.FunctionalTests/Tests/EnlistmentPerFixture/SparseTests.cs
@@ -547,16 +547,18 @@ namespace GVFS.FunctionalTests.Tests.EnlistmentPerFixture
         {
             string pathToChangeFiles = Path.Combine(this.Enlistment.RepoRoot, "GVFS", "GVFS", "CommandLine");
             string modifiedPath = Path.Combine(this.Enlistment.RepoRoot, "GVFS", "GVFS", "Program.cs");
+            string firstFileToDeletePath = Path.Combine(pathToChangeFiles, "CloneHelper.cs");
+            string secondFileToDeletePath = Path.Combine(pathToChangeFiles, "DiagnoseVerb.cs");
             this.fileSystem.WriteAllText(modifiedPath, "New Contents");
             this.fileSystem.WriteAllText(Path.Combine(pathToChangeFiles, "NewHelper.cs"), "New Contents");
-            this.fileSystem.DeleteFile(Path.Combine(pathToChangeFiles, "CloneHelper.cs"));
+            this.fileSystem.DeleteFile(firstFileToDeletePath);
             this.fileSystem.MoveFile(Path.Combine(pathToChangeFiles, "PrefetchHelper.cs"), Path.Combine(pathToChangeFiles, "PrefetchHelperRenamed.cs"));
             this.fileSystem.DeleteDirectory(Path.Combine(this.Enlistment.RepoRoot, "GVFS", "GVFS", "Properties"));
             GitProcess.Invoke(this.Enlistment.RepoRoot, "add .");
 
             this.fileSystem.WriteAllText(Path.Combine(pathToChangeFiles, "NewVerb.cs"), "New Contents");
             this.fileSystem.WriteAllText(Path.Combine(pathToChangeFiles, "CloneVerb.cs"), "New Contents");
-            this.fileSystem.DeleteFile(Path.Combine(pathToChangeFiles, "DiagnoseVerb.cs"));
+            this.fileSystem.DeleteFile(secondFileToDeletePath);
             this.fileSystem.MoveFile(Path.Combine(pathToChangeFiles, "LogVerb.cs"), Path.Combine(pathToChangeFiles, "LogVerbRenamed.cs"));
 
             string expectedStatusOutput = GitProcess.Invoke(this.Enlistment.RepoRoot, "status --porcelain -uall");
@@ -564,6 +566,12 @@ namespace GVFS.FunctionalTests.Tests.EnlistmentPerFixture
             string output = this.gvfsProcess.AddSparseFolders(this.mainSparseFolder);
             output.ShouldContain("Running git status...Succeeded");
             this.ValidateFoldersInSparseList(this.mainSparseFolder);
+
+            firstFileToDeletePath.ShouldNotExistOnDisk(this.fileSystem);
+            secondFileToDeletePath.ShouldNotExistOnDisk(this.fileSystem);
+
+            string interimStatusOutput = GitProcess.Invoke(this.Enlistment.RepoRoot, "status --porcelain -uall");
+            interimStatusOutput.ShouldEqual(expectedStatusOutput, "Status output should not change.");
 
             output = this.gvfsProcess.AddSparseFolders(folders: "Scripts");
             output.ShouldContain("Running git status...Succeeded");
