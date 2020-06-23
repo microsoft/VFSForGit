44714b13234c41cfef8e5166ed4c6e4a8daab6b1
Derrick Stolee <dstolee@microsoft.com>
2020-06-23 07:45:49 -0400

    BuildGVFSForWindows.bat: use nuget on PATH when possible
    Our release build is failing because we cannot download nuget due to TLS
    1.2 changes. But we already require nuget.exe in an earlier step of the
    build.
    
    Require nuget.exe to be on the PATH, and update Readme.me accordingly.
    
    Signed-off-by: Derrick Stolee <dstolee@microsoft.com>

--- Scripts/RestorePackages.bat
+++ Scripts/RestorePackages.bat
@@ -7,10 +7,9 @@ IF "%1"=="" (SET "Configuration=Debug") ELSE (SET "Configuration=%1")
  
 SET SolutionConfiguration=%Configuration%.Windows
 
-SET nuget="%VFS_TOOLSDIR%\nuget.exe"
-IF NOT EXIST %nuget% (
-  mkdir %nuget%\..
-  powershell -ExecutionPolicy Bypass -Command "Invoke-WebRequest 'https://dist.nuget.org/win-x86-commandline/latest/nuget.exe' -OutFile %nuget%"
+FOR /F "tokens=* USEBACKQ" %%F IN (`where nuget.exe`) DO (
+	SET nuget=%%F
+	ECHO Found nuget.exe at '%%F'
 )
 
 dotnet restore %VFS_SRCDIR%\GVFS.sln /p:Configuration=%SolutionConfiguration% /p:VCTargetsPath="C:\Program Files (x86)\MSBuild\Microsoft.Cpp\v4.0\V140" --packages %VFS_PACKAGESDIR% || exit /b 1
