78b4c417922cb8406ebaacdb0316188090ecdbd6
Derrick Stolee <dstolee@microsoft.com>
2020-10-20 14:31:49 -0400

    Delete all macOS projects and platform code
    Some macOS projects were causing failures in the release process, so
    let's finally get around to deleting them.
    
    Signed-off-by: Derrick Stolee <dstolee@microsoft.com>

--- Scripts/Mac/InstallSharedDataQueueStallWorkaround.sh
+++
@@ -1,5 +0,0 @@
-. "$(dirname ${BASH_SOURCE[0]})/InitializeEnvironment.sh"
-
-# DYLD_LIBRARY_PATH contains /usr/local/lib by default, so we'll copy this library there.
-BUILDDIR=$VFS_OUTPUTDIR/GVFS.Build
-cp $BUILDDIR/libSharedDataQueue.dylib /usr/local/lib
