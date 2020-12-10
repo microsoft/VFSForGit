d0357245a2b31e5a00a51b742a6b41ca2ca41192
Gabriel DeBacker <gabrield@microsoft.com>
2020-12-10 22:08:45 +0000

    Add dismount verb

---
+++ GVFS/GVFS/CommandLine/DismountVerb.cs
@@ -0,0 +1,10 @@
+﻿using CommandLine;
+
+namespace GVFS.CommandLine
+{
+    [Verb(DismountVerb.DismountVerbName, HelpText = "Dismount a GVFS virtual repo")]
+    public class DismountVerb: UnmountVerb
+    {
+        private const string DismountVerbName = "dismount";
+    }
+}
