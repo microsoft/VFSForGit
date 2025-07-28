9e04caf269a0cd1b439c54028eb3cc8c289d0768
Matthew John Cheetham <mjcheetham@outlook.com>
2025-07-28 09:58:26 -0700

    ft: install arch-specific msftgit and print OS version
    Install the correct version of Microsoft Git in the functional tests for
    the OS architecture, and print more OS/CPU information for good measure.
    
    Signed-off-by: Matthew John Cheetham <mjcheetham@outlook.com>

--- GVFS/GVFS.Installers/install.bat
+++ GVFS/GVFS.Installers/install.bat
@@ -1,8 +1,18 @@
 @ECHO OFF
 SETLOCAL
 
+REM Determine the correct architecture for the installer
+IF "%PROCESSOR_ARCHITECTURE%"=="AMD64" (
+    SET GIT_ARCH=64-bit
+) ELSE IF "%PROCESSOR_ARCHITECTURE%"=="ARM64" (
+    SET GIT_ARCH=arm64
+) ELSE (
+    ECHO Unknown architecture: %PROCESSOR_ARCHITECTURE%
+    exit 1
+)
+
 REM Lookup full paths to Git and VFS for Git installers
-FOR /F "tokens=* USEBACKQ" %%F IN ( `where /R %~dp0 Git*.exe` ) DO SET GIT_INSTALLER=%%F
+FOR /F "tokens=* USEBACKQ" %%F IN ( `where /R %~dp0 Git*-%GIT_ARCH%.exe` ) DO SET GIT_INSTALLER=%%F
 FOR /F "tokens=* USEBACKQ" %%F IN ( `where /R %~dp0 SetupGVFS*.exe` ) DO SET GVFS_INSTALLER=%%F
 
 REM Create new empty directory for logs
@@ -12,7 +22,7 @@ IF EXIST %LOGDIR% (
 )
 mkdir %LOGDIR%
 
-ECHO Installing Git for Windows...
+ECHO Installing Git (%GIT_ARCH%)...
 %GIT_INSTALLER% /LOG="%LOGDIR%\git.log" /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /ALLOWDOWNGRADE=1
 
 ECHO Installing VFS for Git...
